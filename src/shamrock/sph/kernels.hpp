02f12c10ea1387bd6066901f57b1980504f961df
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-07-31 14:36:53 +0200

    [Doc][Doxygen] SPH kernel doc

--- src/shamrock/sph/kernels.hpp
+++ src/shamrock/sph/kernels.hpp
@@ -102,15 +102,33 @@ namespace shamrock::sph::kernels {
         }
     };
 
+    /**
+     * @brief The M6 SPH kernel
+     * \todo add graph
+     *
+     * @tparam flt_type the flating point representation to use
+     */
     template<class flt_type>
     class M6 {
         public:
         using flt                               = flt_type;
-        inline static constexpr flt_type Rkern  = 3;
-        inline static constexpr flt_type hfactd = 1.0;
+        inline static constexpr flt_type Rkern  = 3;/*!< Radius of the support */
+        inline static constexpr flt_type hfactd = 1.0;/*!< default $h_{\rm fact}$ for this kernel*/
 
+        /**
+         * @brief norm of the kernel
+         * 
+         */
         inline static constexpr flt_type norm = 1 / (120 * shambase::Constants<flt_type>::pi);
 
+        /**
+         * @brief the base function for this SPH kernel
+         * 
+         * Such that : \f$\int f(\vert\vert q\vert\vert) {\rm d}^n \mathbf{q} = 1\f$
+         * Also : \f$ f(q > Rkern) = 0\f$
+         * @param q parameter of the function
+         * @return flt_type the value of \f$ f(q) \f$
+         */
         inline static flt_type f(flt_type q) {
 
             if (q < 1.) {
@@ -124,6 +142,12 @@ namespace shamrock::sph::kernels {
                 return 0;
         }
 
+        /**
+         * @brief derivative of \ref M6.f
+         * 
+         * @param q parameter of the function
+         * @return flt_type the value of \f$ f'(q) \f$
+         */
         inline static flt_type df(flt_type q) {
 
             if (q < 1.) {
@@ -137,6 +161,15 @@ namespace shamrock::sph::kernels {
                 return 0.;
         }
 
+        /**
+         * @brief compute the normed & resized version of the kernel :
+         * \f[
+         *  W(r,h) = C_{\rm norm} \frac{1}{h^3} f(\frac{r}{h})
+         * \f]
+         * @param r 
+         * @param h 
+         * @return flt_type 
+         */
         inline static flt_type W(flt_type r, flt_type h) { return norm * f(r / h) / (h * h * h); }
 
         inline static flt_type dW(flt_type r, flt_type h) {

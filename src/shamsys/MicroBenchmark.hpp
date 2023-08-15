8a2f6efd8f62638e91ac853d8238254c93f6bae5
tdavidcl <timothee.davidcleri@gmail.com>
2023-08-15 17:01:31 +0200

    [Sys] fix license

--- src/shamsys/MicroBenchmark.hpp
+++ src/shamsys/MicroBenchmark.hpp
@@ -1,16 +1,15 @@
 // -------------------------------------------------------//
 //
 // SHAMROCK code for hydrodynamics
-// Copyright(C) 2021-2023 Timothée David--Cléris
-// <timothee.david--cleris@ens-lyon.fr> Licensed under CeCILL 2.1 License, see
-// LICENSE for more information
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
 //
 // -------------------------------------------------------//
 
 #pragma once
 
 namespace shamsys {
-    
+
     /**
     * @brief Run latency & bandwidth benchmark
     * those benchmark where adapted from osu_microbenchmark

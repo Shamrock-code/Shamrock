a1b9f4829c8492907b7647892c5bee6a243122b2
tdavidcl <timothee.davidcleri@gmail.com>
2023-09-29 19:10:23 +0200

    [Cmake] search for any acpp legacy config

--- cmake/ShamConfigureSYCL.cmake
+++ cmake/ShamConfigureSYCL.cmake
@@ -25,49 +25,64 @@ set_property(CACHE SYCL_COMPILER PROPERTY STRINGS DPCPP OPENSYCL OTHER)
 
 
 if(USE_ACPP_CMAKE)
-  find_package(AdaptiveCpp CONFIG REQUIRED)
 
-elseif(USE_HIPSYCL_CMAKE)
-  find_package(hipSYCL CONFIG REQUIRED)
+
+
+  find_package(AdaptiveCpp CONFIG)
+  if(NOT AdaptiveCpp_FOUND)
+    find_package(OpenSYCL CONFIG)
+    if(NOT OpenSYCL_FOUND)
+      find_package(hipSYCL CONFIG)
+      if(NOT hipSYCL_FOUND)
+        message(FATAL_ERROR
+          "You asked shamrock to compiler using
+          the acpp/opensycl/hipsycl cmake integration, 
+          but neither of the cmake packages can be found"        )
+      endif()
+    endif()
+  endif()
+
+
+
 
 else()
 
 
-check_cxx_source_compiles(
+  check_cxx_source_compiles(
     "
     #include <sycl/sycl.hpp>
     int main(void){}
-    " HAS_SYCL2020_HEADER)
+    "    HAS_SYCL2020_HEADER)
 
-if(NOT )
-message(FATAL_ERROR "The sycl header test fail")
-endif()
+  if(NOT HAS_SYCL2020_HEADER)
+    message(FATAL_ERROR "The sycl header test fail")
+  endif()
 
-check_cxx_source_compiles("
+  check_cxx_source_compiles("
 #if (defined(__ACPP__) || defined(__OPENSYCL__))
 int main() { return 0; }
 #else
 not oneAPI
 #endif
-" SYCL_COMPILER_IS_ACPP)
-if (SYCL_COMPILER_IS_ACPP)
+"    SYCL_COMPILER_IS_ACPP)
+  if(SYCL_COMPILER_IS_ACPP)
     set(SYCL_COMPILER "OPENSYCL")
-endif()
+  endif()
 
 
 
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  check_cxx_source_compiles("
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    check_cxx_source_compiles("
                 #include <sycl/sycl.hpp>
                 #if (defined(SYCL_IMPLEMENTATION_ONEAPI))
                 int main() { return 0; }
                 #else
                 not oneAPI
-                #endif" SYCL_COMPILER_IS_DPCPP)
-  if (SYCL_COMPILER_IS_DPCPP)
-    set(SYCL_COMPILER "DPCPP")
+                #endif"      SYCL_COMPILER_IS_DPCPP)
+    if(SYCL_COMPILER_IS_DPCPP)
+      set(SYCL_COMPILER "DPCPP")
+    endif()
   endif()
-endif()
 
 
 
@@ -75,37 +90,37 @@ endif()
 
 
 
-if(NOT DEFINED SYCL_feature_reduc2020)
-message(STATUS "Performing Test " SYCL_feature_reduc2020)
-try_compile(
-    SYCL_feature_reduc2020 ${CMAKE_BINARY_DIR}/compile_tests 
-    ${CMAKE_SOURCE_DIR}/cmake/feature_test/sycl2020_reduc.cpp)
-endif()
-if(SYCL_feature_reduc2020)
+  if(NOT DEFINED SYCL_feature_reduc2020)
+    message(STATUS "Performing Test " SYCL_feature_reduc2020)
+    try_compile(
+      SYCL_feature_reduc2020 ${CMAKE_BINARY_DIR}/compile_tests
+      ${CMAKE_SOURCE_DIR}/cmake/feature_test/sycl2020_reduc.cpp)
+  endif()
+  if(SYCL_feature_reduc2020)
     message(STATUS "Performing Test " SYCL_feature_reduc2020 " - Success")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DSYCL2020_FEATURE_REDUCTION")
-else()
-message(STATUS "Performing Test " SYCL_feature_reduc2020 " - Failed")
-endif()
+  else()
+    message(STATUS "Performing Test " SYCL_feature_reduc2020 " - Failed")
+  endif()
 
 
 
 
 
-if(${SYCL_COMPILER} STREQUAL "DPCPP")
+  if(${SYCL_COMPILER} STREQUAL "DPCPP")
 
-check_cxx_compiler_flag("-fsycl-id-queries-fit-in-int" INTEL_LLVM_HAS_FIT_ID_INT)
-if(INTEL_LLVM_HAS_FIT_ID_INT)
-option(INTEL_LLVM_SYCL_ID_INT32 Off)
-endif()
+    check_cxx_compiler_flag("-fsycl-id-queries-fit-in-int" INTEL_LLVM_HAS_FIT_ID_INT)
+    if(INTEL_LLVM_HAS_FIT_ID_INT)
+      option(INTEL_LLVM_SYCL_ID_INT32 Off)
+    endif()
 
-check_cxx_compiler_flag("-fno-sycl-rdc" INTEL_LLVM_HAS_NO_RDC)
-if(INTEL_LLVM_HAS_NO_RDC)
-option(INTEL_LLVM_NO_RDC Off)
-endif()
+    check_cxx_compiler_flag("-fno-sycl-rdc" INTEL_LLVM_HAS_NO_RDC)
+    if(INTEL_LLVM_HAS_NO_RDC)
+      option(INTEL_LLVM_NO_RDC Off)
+    endif()
 
-elseif(${SYCL_COMPILER} STREQUAL "OPENSYCL")
+  elseif(${SYCL_COMPILER} STREQUAL "OPENSYCL")
 
-endif()
+  endif()
 
 endif()

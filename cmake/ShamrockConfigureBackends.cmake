b734de9f381ce0548f2bce09b9be83d88aa31924
tdavidcl <timothee.davidcleri@gmail.com>
2023-10-04 00:28:04 +0200

    [Cmake] move backend select to cmake module

---
+++ cmake/ShamrockConfigureBackends.cmake
@@ -0,0 +1,21 @@
+## -------------------------------------------------------
+##
+## SHAMROCK code for hydrodynamics
+## Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+## Licensed under CeCILL 2.1 License, see LICENSE for more information
+##
+## -------------------------------------------------------
+
+if("${SHAMROCK_ENABLE_BACKEND}" STREQUAL "SYCL")
+    include(ShamConfigureSYCL)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SHAM_CXX_SYCL_FLAGS} -DSHAMROCK_ENABLE_BACKEND_SYCL")
+elseif("${SHAMROCK_ENABLE_BACKEND}" STREQUAL "KOKKOS")
+    include(ShamConfigureKokkos)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DSHAMROCK_ENABLE_BACKEND_KOKKOS")
+else()
+    message(FATAL_ERROR
+        "You must select a Shamrock Backend "
+        "-DSHAMROCK_ENABLE_BACKEND=[SYCL,KOKKOS]")
+endif()
+set(SHAMROCK_ENABLE_BACKEND "${SHAMROCK_ENABLE_BACKEND}" CACHE STRING "Shamrock backend used")
+

58fb8a0ef33225d7e115b99bc0e9088fac4954b0
Yona <yona.lapeyre@gmail.com>
2023-11-15 06:16:33 +0100

    cleaning

--- exemples/kh_from_phantom.py
+++ exemples/kh_from_phantom.py
@@ -1,10 +1,14 @@
+"""
+Matching the KH to Phantom's
+"""
+
 import shamrock
 import matplotlib.pyplot as plt
 import numpy as np
 
-outputdir = "/home/ylapeyre/hydro/sham_KH_from_ph/"
-ph_dir = "/home/ylapeyre/hydro/phantom_KH/"
-ph_file = ph_dir + "kh_00000"
+outputdir = "your_output_directory/"
+ph_dir = "directory_of_phantom_dumps/"
+ph_file = ph_dir + "dumpname_00000"
 
 ctx = shamrock.Context()
 ctx.pdata_layout_new()
@@ -17,7 +21,7 @@ cfg.set_boundary_periodic()
 cfg.print_status()
 
 model.set_solver_config(cfg)
-pmass = 4.0406753833390151E-006
+pmass = 4.0406753833390151E-006 # valid for basic phantom setup. Otherwise read it in th header of the phantom dump file
 model.set_particle_mass(pmass)
 model.init_scheduler(int(1e5),1)
 
@@ -29,10 +33,7 @@ t_sum = 0
 t_target = 4.
 current_dt = 0.002 #ev_dic['dt'][0]
 while t_sum < t_target:
-#for next_dt in ev_dic['dt'][1:]:
 
-    #print("step : t=",t_sum)
-    
     next_dt = model.evolve(t_sum,current_dt, True, outputdir + "dump_"+str(i_dump)+".vtk", True)
 
     if i % 1 == 0:

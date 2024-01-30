e9186fd373afd455b6ee8e96ae51429c5810ba62
Yona LAPEYRE <118538359+y-lapeyre@users.noreply.github.com>
2024-01-30 14:21:23 +0100

    fix path

--- exemples/disc_isothermal.py
+++ exemples/disc_isothermal.py
@@ -2,7 +2,7 @@ import shamrock
 import matplotlib.pyplot as plt
 import numpy as np
 
-outputdir = "/local/ylapeyre/Shamrock_tests/30_01/"
+outputdir = ""
 
 si = shamrock.UnitSystem()
 sicte = shamrock.Constants(si)

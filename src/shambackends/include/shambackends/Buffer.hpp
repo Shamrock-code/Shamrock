fdf416782155b3f25b99ef195b99fb8ddfe0f2c3
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2024-02-06 17:48:39 +0100

    [Backend] cleaner buffer alloc module

--- src/shambackends/include/shambackends/Buffer.hpp
+++ src/shambackends/include/shambackends/Buffer.hpp
@@ -19,53 +19,40 @@
 namespace sham::buffer::details {
 
     template<class T>
-    class BufferImplBasic {
+    class BufferAllocBasic {
 
         QueueId queue_id;
         std::unique_ptr<sycl::buffer<T>> storage;
 
         public:
         inline QueueId get_queue_id() { return queue_id; }
+
         inline sycl::queue &get_queue() { queue_id.get_queue(); }
 
+        BufferAllocBasic(u64 size, QueueId qid) : queue_id(qid) {
+            storage = std::make_unique<sycl::buffer<T>>(size);
+        }
 
-        bool is_allocated() { return bool(storage); }
-
-        inline u64 get_capacity() { return (is_allocated()) ? storage->size() : 0; }
-
-        void alloc(u64 min_size) { storage = std::make_unique<sycl::buffer<T>>(min_size); }
-
-        void free() { storage.reset(); }
-
-        inline const std::unique_ptr<sycl::buffer<T>> &get_buf() const { return storage; }
-
+        inline sycl::buffer<T> &get_buf() const { return *storage; }
     };
 
 } // namespace sham::buffer::details
 
 namespace sham {
 
-    template<class T, class Impl = buffer::details::BufferImplBasic<T>>
+    template<class T, class AllocImpl = buffer::details::BufferAllocBasic<T>>
     class Buffer {
 
-        Impl impl;
+        AllocImpl impl;
 
         public:
+        Buffer(u64 size, QueueId qid) : impl(size, qid) {}
 
         inline QueueId get_queue_id() { return impl.get_queue_id(); }
 
         inline sycl::queue &get_queue() { return impl.get_queue(); }
 
-        bool is_allocated() { return impl.is_allocated(); }
-
-        inline u64 get_capacity() { return impl.get_capacity(); }
-
-        void alloc(u64 min_size) { impl.alloc(min_size); }
-
-        void free() { impl.free(); }
-
-        inline const std::unique_ptr<sycl::buffer<T>> &get_buf() const { return impl.get_buf(); }
-
+        inline sycl::buffer<T> &get_buf() const { return impl.get_buf(); }
     };
 
 } // namespace sham

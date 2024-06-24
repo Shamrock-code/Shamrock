5b63a90f9a64f971005f80a2fd234ec528f3d670
tdavidcl <timothee.davidcleri@gmail.com>
2024-06-24 18:46:30 +0200

    [Backends][Test] add missing license

--- src/tests/shambackends/DeviceBufferTests.cpp
+++ src/tests/shambackends/DeviceBufferTests.cpp
@@ -1,3 +1,10 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
 
 #include "shambackends/USMPtrHolder.hpp"
 #include "shamsys/NodeInstance.hpp"
@@ -52,7 +59,7 @@ void add(sycl::queue & q,sham::DeviceBuffer<int> & f_a,sham::DeviceBuffer<int> &
 TestStart(Unittest, "shambackends/DeviceBuffer:smalltaskgraph", DeviceBuffer_small_task_graph, 1){
 
     std::shared_ptr<sham::DeviceScheduler> dev_sched = shamsys::instance::get_compute_scheduler_ptr();
-    
+
     sham::DeviceBuffer<int> a{1000,dev_sched};
     sham::DeviceBuffer<int> b{1000,dev_sched};
     sham::DeviceBuffer<int> c{1000,dev_sched};

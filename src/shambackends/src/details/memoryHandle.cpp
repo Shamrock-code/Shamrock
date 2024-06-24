60fe42e56f601c7da827f3ef131c091e5e485e38
tdavidcl <timothee.davidcleri@gmail.com>
2024-06-24 18:37:47 +0200

    [Backends] group memory handling in templates

--- src/shambackends/src/details/memoryHandle.cpp
+++ src/shambackends/src/details/memoryHandle.cpp
@@ -14,73 +14,73 @@
  */
 
 #include "shambackends/USMPtrHolder.hpp"
+#include "shamcomm/logs.hpp"
 #include <shambackends/details/memoryHandle.hpp>
 
 namespace sham::details {
 
-    template<>
-    USMPtrHolder<device>
-    create_usm_ptr<device>(u32 size, std::shared_ptr<DeviceScheduler> dev_sched) {
+    template<USMKindTarget target>
+    std::string get_mode_name();
 
-        std::cout << "create usm pointer size : " << size << " | mode = device" << std::endl;
-        auto ret = USMPtrHolder<device>(size, dev_sched);
-        std::cout << "pointer created : ptr = " << ret.get_raw_ptr() << std::endl;
+    template<>
+    std::string get_mode_name<device>() {
+        return "device";
+    }
+
+    template<>
+    std::string get_mode_name<shared>() {
+        return "shared";
+    }
+
+    template<>
+    std::string get_mode_name<host>() {
+        return "host";
+    }
+
+    template<USMKindTarget target>
+    USMPtrHolder<target> create_usm_ptr(u32 size, std::shared_ptr<DeviceScheduler> dev_sched) {
+        shamcomm::logs::debug_alloc_ln(
+            "memoryHandle",
+            "create usm pointer size :",
+            size,
+            " | mode =",
+            get_mode_name<target>());
+        auto ret = USMPtrHolder<target>(size, dev_sched);
+        shamcomm::logs::debug_alloc_ln(
+            "memoryHandle", "pointer created : ptr =", ret.get_raw_ptr());
         return ret;
     }
 
-    template<>
-    USMPtrHolder<shared>
-    create_usm_ptr<shared>(u32 size, std::shared_ptr<DeviceScheduler> dev_sched) {
+    template<USMKindTarget target>
+    void
+    release_usm_ptr(USMPtrHolder<target> &&usm_ptr_hold, details::BufferEventHandler &&events) {
 
-        std::cout << "create usm pointer size : " << size << " | mode = shared" << std::endl;
-        auto ret = USMPtrHolder<shared>(size, dev_sched);
-        std::cout << "pointer created : ptr = " << ret.get_raw_ptr() << std::endl;
-        return ret;
-    }
-
-    template<>
-    USMPtrHolder<host> create_usm_ptr<host>(u32 size, std::shared_ptr<DeviceScheduler> dev_sched) {
-
-        std::cout << "create usm pointer size : " << size << " | mode = host" << std::endl;
-        auto ret = USMPtrHolder<host>(size, dev_sched);
-        std::cout << "pointer created : ptr = " << ret.get_raw_ptr() << std::endl;
-        return ret;
-    }
-
-    template<>
-    void release_usm_ptr<device>(USMPtrHolder<device> &&usm_ptr_hold, BufferEventHandler &&events) {
-
-        std::cout << "release usm pointer size : " << usm_ptr_hold.get_size()
-                  << " | ptr = " << usm_ptr_hold.get_raw_ptr() << " | mode = device"
-                  << std::endl;
-        std::cout << "waiting event completion ..." << std::endl;
+        shamcomm::logs::debug_alloc_ln(
+            "memoryHandle",
+            "release usm pointer size :",
+            usm_ptr_hold.get_size(),
+            " | ptr =",
+            usm_ptr_hold.get_raw_ptr(),
+            " | mode =",
+            get_mode_name<target>());
+        shamcomm::logs::debug_alloc_ln("memoryHandle", "waiting event completion ...");
         events.wait_all();
-        std::cout << "done, freeing memory" << std::endl;
+        shamcomm::logs::debug_alloc_ln("memoryHandle", "done, freeing memory");
         usm_ptr_hold.free_ptr();
     }
 
-    template<>
-    void release_usm_ptr<shared>(USMPtrHolder<shared> &&usm_ptr_hold, BufferEventHandler &&events) {
+    template USMPtrHolder<device> create_usm_ptr<device>(u32 size, std::shared_ptr<DeviceScheduler> dev_sched);
+    template USMPtrHolder<shared> create_usm_ptr<shared>(u32 size, std::shared_ptr<DeviceScheduler> dev_sched);
+    template USMPtrHolder<host> create_usm_ptr<host>(u32 size, std::shared_ptr<DeviceScheduler> dev_sched);
 
-        std::cout << "release usm pointer size : " << usm_ptr_hold.get_size()
-                  << " | ptr = " << usm_ptr_hold.get_raw_ptr() << " | mode = shared"
-                  << std::endl;
-        std::cout << "waiting event completion ..." << std::endl;
-        events.wait_all();
-        std::cout << "done, freeing memory" << std::endl;
-        usm_ptr_hold.free_ptr();
-    }
-
-    template<>
-    void release_usm_ptr<host>(USMPtrHolder<host> &&usm_ptr_hold, BufferEventHandler &&events) {
-
-        std::cout << "release usm pointer size : " << usm_ptr_hold.get_size()
-                  << " | ptr = " << usm_ptr_hold.get_raw_ptr() << " | mode = host"
-                  << std::endl;
-        std::cout << "waiting event completion ..." << std::endl;
-        events.wait_all();
-        std::cout << "done, freeing memory" << std::endl;
-        usm_ptr_hold.free_ptr();
-    }
+    template 
+    void
+    release_usm_ptr<device>(USMPtrHolder<device> &&usm_ptr_hold, details::BufferEventHandler &&events);
+    template 
+    void
+    release_usm_ptr<shared>(USMPtrHolder<shared> &&usm_ptr_hold, details::BufferEventHandler &&events);
+    template 
+    void
+    release_usm_ptr<host>(USMPtrHolder<host> &&usm_ptr_hold, details::BufferEventHandler &&events);
 
 } // namespace sham::details

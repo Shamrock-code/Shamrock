bdf45c71aa5b10b4f6271fd697a5483332885392
Akos299 <113362281+Akos299@users.noreply.github.com>
2024-07-19 16:24:01 +0200

    Update riemann.hpp
    [references] Riemann solver fix

--- src/shammath/include/shammath/riemann.hpp
+++ src/shammath/include/shammath/riemann.hpp
@@ -203,12 +203,14 @@ namespace shammath {
 
         const auto csL = sound_speed(primL, gamma);
         const auto csR = sound_speed(primR, gamma);
-
+        
+        // Equation (10.56) from Toro 3rd Edition , Springer 2009
         const auto S = sham::max( (sham::abs(primL.vel[0]) + csL), (sham::abs(primR.vel[0]) + csR) );
 
         const auto fL = hydro_flux_x(cL, gamma);
         const auto fR = hydro_flux_x(cR, gamma);
-
+        
+        // Equation (10.55) from Toro 3rd Edition , Springer 2009 
         return 0.5 * ((fL + fR) - (cR - cL) * S);
     }
 
@@ -305,13 +307,14 @@ namespace shammath {
         //const auto S_L = sham::min(primL.vel[0], primR.vel[0]) - sham::max(csL, csR);
         //const auto S_R = sham::max(primL.vel[0], primR.vel[0]) + sham::max(csL, csR);
 
-        // Toro form
+        // Toro form Equation (10.48)
         const auto S_L = sham::min(primL.vel[0] - csL, primR.vel[0] - csR);
         const auto S_R = sham::max(primL.vel[0] + csL, primR.vel[0] + csR);
 
         const auto fluxL = hydro_flux_x(consL, gamma);
         const auto fluxR = hydro_flux_x(consR, gamma);
 
+        // Equation (10.26) from Toro 3rd Edition , Springer 2009
         auto hll_flux = [=]() {
             if( S_L >= 0)
                 return fluxL;

192bc58249dfe77a2243a0403a3fbf5cc31a50b4
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-08-07 18:31:38 +0200

    [Models] document eos configs and change q sign for discs

--- src/shamphys/include/shamphys/eos_config.hpp
+++ src/shamphys/include/shamphys/eos_config.hpp
@@ -24,14 +24,31 @@ namespace shamphys {
      * @brief Configuration struct for adiabatic equation of state
      *
      * @tparam Tscal Scalar type
+     *
+     * This struct holds the configuration for the adiabatic equation of state.
+     * It contains the adiabatic index, which is a dimensionless quantity that
+     * determines the behavior of the gas.
+     *
+     * The equation of state is given by:
+     * \f$ p = \rho^\gamma \f$
      */
     template<class Tscal>
     struct EOS_Config_Adiabatic {
+        /// Adiabatic index
         Tscal gamma;
     };
 
     /**
-     * @brief equal operator for EOS_Config_Adiabatic
+     * @brief Equal operator for the EOS_Config_Adiabatic struct
+     *
+     * @tparam Tscal Scalar type
+     * @param lhs First EOS_Config_Adiabatic struct to compare
+     * @param rhs Second EOS_Config_Adiabatic struct to compare
+     *
+     * This function checks if two EOS_Config_Adiabatic structs are equal by comparing their
+     * gamma values.
+     *
+     * @return true if the two structs have the same gamma value, false otherwise
      */
     template<class Tscal>
     inline bool
@@ -40,19 +57,37 @@ namespace shamphys {
     }
 
     /**
-     * @brief Configuration struct for locally isothermal equation of state from Lodato Price 2007
+     * @brief Configuration struct for the locally isothermal equation of state from Lodato Price
+     * 2007
      *
      * @tparam Tscal Scalar type
+     *
+     * The equation of state is given by:
+     * \f$ p = c_{s,0}^2 (r / r_0)^{-q} \rho \f$
      */
     template<class Tscal>
     struct EOS_Config_LocallyIsothermal_LP07 {
+        /// Soundspeed at the reference radius
         Tscal cs0 = 0.005;
-        Tscal q   = -2;
-        Tscal r0  = 10;
+
+        /// Power exponent of the soundspeed profile
+        Tscal q = 2;
+
+        /// Reference radius
+        Tscal r0 = 10;
     };
 
     /**
-     * @brief equal operator for EOS_Config_LocallyIsothermal_LP07
+     * @brief Equal operator for the EOS_Config_LocallyIsothermal_LP07 struct
+     *
+     * @tparam Tscal Scalar type
+     * @param lhs First EOS_Config_LocallyIsothermal_LP07 struct to compare
+     * @param rhs Second EOS_Config_LocallyIsothermal_LP07 struct to compare
+     *
+     * This function checks if two EOS_Config_LocallyIsothermal_LP07 structs are equal by
+     * comparing their cs0, q, and r0 values.
+     *
+     * @return true if the two structs have the same cs0, q, and r0 values, false otherwise
      */
     template<class Tscal>
     inline bool operator==(

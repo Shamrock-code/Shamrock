523851aa7922437419b4fc29d57277d6f8128e0d
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-08-07 18:44:45 +0200

    [Models] document EosConfig

--- src/shammodels/EOSConfig.hpp
+++ src/shammodels/EOSConfig.hpp
@@ -30,39 +30,67 @@
 
 namespace shammodels {
 
+    /**
+     * @brief Configuration struct for the equation of state used in the hydrodynamic models
+     *
+     * @tparam Tvec The vector type used to store the physical quantities (e.g., velocity,
+     * pressure, etc.)
+     */
     template<class Tvec>
     struct EOSConfig {
+        /// Scalar type associated to the vector template type
+        using Tscal = shambase::VecComponent<Tvec>;
 
-        using Tscal              = shambase::VecComponent<Tvec>;
+        /// Dimension of the vector quantities
         static constexpr u32 dim = shambase::VectorProperties<Tvec>::dimension;
 
         // EOS types definition usable in the code
 
+        /// Adiabatic equation of state configuration
         using Adiabatic = shamphys::EOS_Config_Adiabatic<Tscal>;
 
+        /// Locally isothermal equation of state configuration
         struct LocallyIsothermal {};
 
-
+        /// Locally isothermal equation of state configuration from Lodato Price 2007
         using LocallyIsothermalLP07 = shamphys::EOS_Config_LocallyIsothermal_LP07<Tscal>;
 
-        // internal wiring of the eos to the code
-
+        /// Variant type to store the EOS configuration
         using Variant = std::variant<Adiabatic, LocallyIsothermal, LocallyIsothermalLP07>;
 
+        /// Current EOS configuration
         Variant config = Adiabatic{};
 
-        /// Set the config to an adiabatic equation of state
+        /**
+         * @brief Set the EOS configuration to an adiabatic equation of state
+         *
+         * @param gamma The adiabatic index
+         */
         inline void set_adiabatic(Tscal gamma) { config = Adiabatic{gamma}; }
 
-        /// Set the config to a locally isothermal equation of state
+        /**
+         * @brief Set the EOS configuration to a locally isothermal equation of state
+         */
         inline void set_locally_isothermal() { config = LocallyIsothermal{}; }
 
-        /// Set the config to a locally isothermal (Lodato Price 2007) equation of state
+        /**
+         * @brief Set the EOS configuration to a locally isothermal equation of state
+         * (Lodato Price 2007)
+         *
+         * The equation of state is given by:
+         * \f$ p = c_{s,0}^2 (r / r_0)^{-q} \rho \f$
+         *
+         * @param cs0 Soundspeed at the reference radius
+         * @param q Power exponent of the soundspeed profile
+         * @param r0 Reference radius
+         */
         inline void set_locally_isothermalLP07(Tscal cs0, Tscal q, Tscal r0) {
             config = LocallyIsothermalLP07{cs0, q, r0};
         }
 
-        /// Print current status of the EOSConfig
+        /**
+         * @brief Print current status of the EOSConfig
+         */
         inline void print_status();
     };
 
@@ -98,8 +126,17 @@ namespace shammodels {
     /**
      * @brief Serialize EOSConfig to json
      *
-     * This function is using the following JSON format:
-     * \todo
+     * This function serializes an EOSConfig to a json object with the following format:
+     * \code{.json}
+     * {
+     *    "Tvec": "f32_3"|"f64_3",
+     *    "eos_type": "adiabatic"|"locally_isothermal"|"locally_isothermal_lp07",
+     *    "gamma"?: number,
+     *    "cs0"?: number,
+     *    "q"?: number,
+     *    "r0"?: number
+     * }
+     * \endcode
      *
      * @param j json object
      * @param p EOSConfig to serialize
@@ -147,8 +184,8 @@ namespace shammodels {
      * @param j The JSON object to deserialize
      * @param p The EOSConfig<Tvec> to deserialize to
      *
-     * This function is using the following JSON format:
-     * \todo
+     * @see shammodels::to_json(nlohmann::json &j, const EOSConfig<Tvec> &p) for details of the
+     * format
      *
      * Throws an std::runtime_error if the JSON object is not in the expected format
      */

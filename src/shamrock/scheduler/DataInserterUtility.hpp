bb7ae31223b6a25730667f807f7223db9096e1d9
tdavidcl <timothee.davidcleri@gmail.com>
2024-08-25 00:36:52 +0200

    [Scheduler] base of a utility to insert data in the scheduler

---
+++ src/shamrock/scheduler/DataInserterUtility.hpp
@@ -0,0 +1,70 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file DataInserterUtility.hpp
+ * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
+ * @brief
+ */
+
+#include "shamrock/patch/PatchData.hpp"
+#include "shamrock/scheduler/PatchScheduler.hpp"
+#include "shamrock/scheduler/ReattributeDataUtility.hpp"
+#include "shamrock/scheduler/SerialPatchTree.hpp"
+
+namespace shamrock {
+
+    class DataInserterUtility {
+        PatchScheduler &sched;
+
+        public:
+        DataInserterUtility(PatchScheduler &sched) : sched(sched) {}
+
+        /**
+         * @brief Pushes data into the scheduler.
+         *
+         * @warning This function must be called by all MPI ranks
+         *
+         * @param pdat The PatchData object containing the data to be inserted.
+         * @param main_field_name The name of the main field.
+         * @param split_threshold The threshold at which the data will be split.
+         *
+         * @todo use directly main field at id=0 and deduce type
+         * @todo implement case we more object than the threshold are present
+         */
+        template<class Tvec>
+        void push_patch_data(
+            shamrock::patch::PatchData &pdat_ins,
+            std::string main_field_name,
+            u32 split_threshold) {
+            using namespace shamrock::patch;
+
+            u32 pdat_ob_cnt = pdat_ins.get_obj_cnt();
+
+            if (pdat_ob_cnt < split_threshold) {
+                sched.for_each_local_patchdata([&](const Patch p, PatchData &pdat) {
+                    pdat.insert_elements(pdat_ins);
+                    return; // We insert only in first patch (no duplicates)
+                });
+            } else {
+                shambase::throw_unimplemented("Not implemented yet please keep the obj count to be "
+                                              "inserted below the split_threshold, sorrrrrry ...");
+            }
+
+            // move data into the corect patches
+            SerialPatchTree<Tvec> sptree(sched.patch_tree);
+            ReattributeDataUtility reatrib(sched);
+            sptree.attach_buf();
+            reatrib.reatribute_patch_objects(sptree, main_field_name);
+            sched.check_patchdata_locality_corectness();
+        }
+    };
+
+} // namespace shamrock

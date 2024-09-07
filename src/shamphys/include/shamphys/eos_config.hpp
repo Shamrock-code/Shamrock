99ee56074290f356394f73bdc00a1a5956319bf0
tdavidcl <timothee.davidcleri@gmail.com>
2024-09-07 13:51:04 +0200

    [SPH] clarify notations

--- src/shamphys/include/shamphys/eos_config.hpp
+++ src/shamphys/include/shamphys/eos_config.hpp
@@ -101,8 +101,11 @@ namespace shamphys {
      *
      * @tparam Tscal Scalar type
      *
+     * Note that the notation in the original paper are confusing and a clearer version is to use
+     * the form in The Santa Barbara Binary−disk Code Comparison, Duffel et al. 2024
+     *
      * The equation of state is given by:
-     * \f$ c_s = (H/r) \sqrt(- \phi_{\rm grav}) \f$
+     * \f$ c_s = (H(r)/r) \sqrt(- \phi_{\rm grav}) \f$
      */
     template<class Tscal>
     struct EOS_Config_LocallyIsothermalDisc_Farris2014 {

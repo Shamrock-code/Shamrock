5246e85697bdab8b7f768e037e21c2e940858f27
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-10-04 18:27:48 +0200

    update runscripts

--- exemples/ci_tests/reload_old_dump.py
+++ exemples/ci_tests/reload_old_dump.py
@@ -5,4 +5,4 @@ ctx.pdata_layout_new()
 
 model = shamrock.get_SPHModel(context = ctx, vector_type = "f64_3",sph_kernel = "M4")
 
-model.load_from_dump("reference-files/santa_barbara_100K_0000.sham")
+model.load_from_dump("reference-files/old_sham_dump/santa_barbara_100K_0000.sham")

5a9335918c7770f6582ec2f26c283f1f486b52c1
tdavidcl <timothee.davidcleri@gmail.com>
2024-10-19 10:20:37 +0200

    [Profiling] add some doxygen doc

--- src/shambase/include/shambase/profiling/profiling.hpp
+++ src/shambase/include/shambase/profiling/profiling.hpp
@@ -23,17 +23,60 @@
 
 namespace shambase::profiling {
 
+    /**
+     * @brief Set wether to enable NVTX profiling
+     *
+     * @param enable_nvtx Whether to enable NVTX profiling
+     */
     void set_enable_nvtx(bool enable_nvtx);
+
+    /**
+     * @brief Set wether to enable profiling
+     *
+     * @param enable_profiling Whether to enable profiling
+     */
     void set_enable_profiling(bool enable_profiling);
+
+    /**
+     * @brief Use complete event, or start and begin event in chrome tracing
+     *
+     * @param use_complete_event Whether to use complete events
+     */
     void set_use_complete_event(bool use_complete_event);
+
+    /**
+     * @brief Set the event record threshold
+     *
+     * The threshold is the minimum time in seconds between two events, if the time between two
+     * events is smaller than the threshold, the second event will not be recorded.
+     *
+     * @param threshold The event record threshold
+     */
     void set_event_record_threshold(f64 threshold);
 
+    /**
+     * @brief Register the start of a profiling event
+     *
+     * @param fileloc The source location of the function
+     * @param t_start The starting time of the event
+     * @param name The name of the event
+     * @param category_name The name of the category
+     */
     void stack_entry_start(
         const SourceLocation &fileloc,
         f64 t_start,
         std::optional<std::string> name          = std::nullopt,
         std::optional<std::string> category_name = std::nullopt);
 
+    /**
+     * @brief Register the end of a profiling event
+     *
+     * @param fileloc The source location of the function
+     * @param t_start The starting time of the event
+     * @param tend The ending time of the event
+     * @param name The name of the event
+     * @param category_name The name of the category
+     */
     void stack_entry_end(
         const SourceLocation &fileloc,
         f64 t_start,
@@ -41,16 +84,37 @@ namespace shambase::profiling {
         std::optional<std::string> name          = std::nullopt,
         std::optional<std::string> category_name = std::nullopt);
 
+    /**
+     * @brief Start a profiling event without a time info
+     *
+     * @param fileloc The source location of the function
+     * @param name The name of the event
+     * @param category_name The name of the category
+     */
     void stack_entry_start_no_time(
         const SourceLocation &fileloc,
         std::optional<std::string> name          = std::nullopt,
         std::optional<std::string> category_name = std::nullopt);
 
+    /**
+     * @brief End a profiling event without a time info
+     *
+     * @param fileloc The source location of the function
+     * @param name The name of the event
+     * @param category_name The name of the category
+     */
     void stack_entry_end_no_time(
         const SourceLocation &fileloc,
         std::optional<std::string> name          = std::nullopt,
         std::optional<std::string> category_name = std::nullopt);
 
+    /**
+     * @brief Register a counter value
+     *
+     * @param name The name of the counter
+     * @param time The time of the event
+     * @param val The value of the counter
+     */
     void register_counter_val(const std::string &name, f64 time, f64 val);
 
 } // namespace shambase::profiling

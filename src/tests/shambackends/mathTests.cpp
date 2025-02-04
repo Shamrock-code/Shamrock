5b739bf610456c0986fed470c05f0f7a1d693ca7
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-02-04 01:10:02 +0100

    add 0 case to test

--- src/tests/shambackends/mathTests.cpp
+++ src/tests/shambackends/mathTests.cpp
@@ -53,6 +53,7 @@ TestStart(Unittest, "shambackends/math.hpp:inv_sat", shambackendsmathinv_sat, 1)
     REQUIRE_EQUAL(sham::inv_sat<f64>(-1.e-9_f64), -1._f64 / 1.e-9_f64);
     REQUIRE_EQUAL(sham::inv_sat<f64>(-1.e-10_f64), 0._f64);
 
+    REQUIRE_EQUAL(sham::inv_sat<f64>(0._f64), 0._f64);
     REQUIRE_EQUAL(sham::inv_sat<f64>(nan_val), 0._f64);
 }
 
@@ -70,6 +71,7 @@ TestStart(Unittest, "shambackends/math.hpp:inv_sat_positive", shambackendsmathin
     REQUIRE_EQUAL(sham::inv_sat_positive<f64>(1.e-9_f64), 1._f64 / 1.e-9_f64);
     REQUIRE_EQUAL(sham::inv_sat_positive<f64>(1.e-10_f64), 0._f64);
 
+    REQUIRE_EQUAL(sham::inv_sat_positive<f64>(0._f64), 0._f64);
     REQUIRE_EQUAL(sham::inv_sat_positive<f64>(nan_val), 0._f64);
 }
 

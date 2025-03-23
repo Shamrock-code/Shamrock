c4d1f0854e722ddc8451f9494fa3b9a4fd4bf652
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-03-23 18:19:42 +0100

    remove old pull_reffile function

--- env/machine/conda/acpp/conda_acpp_env.sh
+++ env/machine/conda/acpp/conda_acpp_env.sh
@@ -40,12 +40,6 @@ function shammake {
     (cd $BUILD_DIR && $MAKE_EXEC "${MAKE_OPT[@]}" "${@}")
 }
 
-export REF_FILES_PATH=$BUILD_DIR/reference-files
-
-function pull_reffiles {
-    git clone https://github.com/Shamrock-code/reference-files.git $REF_FILES_PATH
-}
-
 function update_env {
     conda env update --file environment.yml --prune
 }

2480882e2fb03a8f022fef7e11796572d25b109e
Yona Lapeyre <yona.lapeyre@ens-lyon.fr>
2025-05-06 10:09:52 +0200

    Do the setup if there is no dump, load otherwise

--- exemples/godunov_sedov.py
+++ exemples/godunov_sedov.py
@@ -1,5 +1,3 @@
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -41,6 +39,33 @@ codeu = shamrock.UnitSystem(
 ucte = shamrock.Constants(codeu)
 
 
+################# DUMP HANDLING ##################
+def get_last_dump():
+    import glob
+
+    res = glob.glob(outputdir + "sedov_" + "*.sham")
+
+    f_max = ""
+    num_max = -1
+
+    for f in res:
+        try:
+            dump_num = int(f[len("sedov_") : -5])
+            if dump_num > num_max:
+                f_max = f
+                num_max = dump_num
+        except:
+            pass
+
+    if num_max == -1:
+        return None
+    else:
+        return num_max
+
+
+idump_last_dump = get_last_dump()
+
+
 ##################### SETUP ######################
 def rho_map(rmin, rmax):
     return 1.0
@@ -66,27 +91,31 @@ ctx = shamrock.Context()
 ctx.pdata_layout_new()
 model = shamrock.get_Model_Ramses(context=ctx, vector_type="f64_3", grid_repr="i64_3")
 
-model.load_from_dump("sedov_0000.sham")
-# model.init_scheduler(int(1e7), 1)
-# model.make_base_grid((0, 0, 0), (sz, sz, sz), (base * multx, base * multy, base * multz))
-# cfg = model.gen_default_config()
-# cfg.set_scale_factor(scale_fact)
-# cfg.set_eos_gamma(gamma)
-# cfg.set_riemann_solver_hllc()
-# cfg.set_slope_lim_vanleer_sym()
-# cfg.set_face_time_interpolation(True)
-# cfg.set_Csafe(C_cour)
-# model.set_config(cfg)
-# model.set_field_value_lambda_f64("rho", rho_map)
-# model.set_field_value_lambda_f64("rhoetot", rhoe_map)
-# model.set_field_value_lambda_f64_3("rhovel", rhovel_map)
+if idump_last_dump is not None:
+    model.load_from_dump(outputdir + "sedov_" + f"{idump_last_dump:04}" + ".sham")
+    idump = idump_last_dump + 1  # avoid overwriting your start dump !
+else:
+    idump = 0
+    model.init_scheduler(int(1e7), 1)
+    model.make_base_grid((0, 0, 0), (sz, sz, sz), (base * multx, base * multy, base * multz))
+    cfg = model.gen_default_config()
+    cfg.set_scale_factor(scale_fact)
+    cfg.set_eos_gamma(gamma)
+    cfg.set_riemann_solver_hllc()
+    cfg.set_slope_lim_vanleer_sym()
+    cfg.set_face_time_interpolation(True)
+    cfg.set_Csafe(C_cour)
+    model.set_config(cfg)
+    model.set_field_value_lambda_f64("rho", rho_map)
+    model.set_field_value_lambda_f64("rhoetot", rhoe_map)
+    model.set_field_value_lambda_f64_3("rhovel", rhovel_map)
+
 
 ################## SIMULATION ####################
 t = 0
-i = 0
 while t <= tmax:
-    # model.dump_vtk(outputdir + "sedov_" + f"{i:04}" + ".vtk")
-    model.dump(outputdir + "sedov_" + f"{i:04}" + ".sham")
+    # model.dump_vtk(outputdir + "sedov_" + f"{idump:04}" + ".vtk")
+    model.dump(outputdir + "sedov_" + f"{idump:04}" + ".sham")
     model.evolve_until(t)
     t += dt_dump
-    i += 1
+    idump += 1

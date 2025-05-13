695fe7b678a89abee01dfc0c54c4b40230888fcd
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-05-13 13:17:20 +0200

    weird missing flags

--- env/machine/argonne/aurora/env_oneapi.sh
+++ env/machine/argonne/aurora/env_oneapi.sh
@@ -16,6 +16,7 @@ function shamconfigure {
         -DCMAKE_CXX_COMPILER=$(which icpx) \
         -DCMAKE_C_COMPILER=$(which icx) \
         -DCMAKE_CXX_FLAGS="-fsycl -fp-model=precise" \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,--copy-dt-needed-entries" \
         -DCMAKE_BUILD_TYPE="${SHAMROCK_BUILD_TYPE}" \
         -DBUILD_TEST=Yes \
         "${CMAKE_OPT[@]}"

c2c7bae4e1f9901b0b304804400387fe4ce37715
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-06-09 22:27:03 +0200

    fix intel/oneapi build

--- src/shamtree/src/MortonCodeSet.cpp
+++ src/shamtree/src/MortonCodeSet.cpp
@@ -149,3 +149,19 @@ template shamtree::MortonCodeSet<u64, f64_3, 3> shamtree::morton_code_set_from_p
     sham::DeviceBuffer<f64_3> &pos_buf,
     u32 cnt_obj,
     u32 morton_count);
+
+template shamtree::MortonCodeSet<u32, f64_3, 3> shamtree::morton_code_set_from_positions(
+    const sham::DeviceScheduler_ptr &dev_sched,
+    shammath::AABB<f64_3> bounding_box,
+    sham::DeviceBuffer<f64_3> &pos_buf,
+    u32 cnt_obj,
+    u32 morton_count,
+    sham::DeviceBuffer<u32> &&cache_buf_morton_codes);
+
+template shamtree::MortonCodeSet<u64, f64_3, 3> shamtree::morton_code_set_from_positions(
+    const sham::DeviceScheduler_ptr &dev_sched,
+    shammath::AABB<f64_3> bounding_box,
+    sham::DeviceBuffer<f64_3> &pos_buf,
+    u32 cnt_obj,
+    u32 morton_count,
+    sham::DeviceBuffer<u64> &&cache_buf_morton_codes);

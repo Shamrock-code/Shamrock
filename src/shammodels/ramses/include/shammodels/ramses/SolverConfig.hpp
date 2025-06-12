0a408d179f6f0b44463671bf145e6b4eb9e2c260
bcommerc <benoit.commercon@ens-lyon.fr>
2025-06-12 18:00:44 +0200

    Update src/shammodels/ramses/include/shammodels/ramses/SolverConfig.hpp
    Co-authored-by: David--Cléris Timothée <timothee.davidcleris@proton.me>

--- src/shammodels/ramses/include/shammodels/ramses/SolverConfig.hpp
+++ src/shammodels/ramses/include/shammodels/ramses/SolverConfig.hpp
@@ -86,6 +86,7 @@ namespace shammodels::basegodunov {
 
         inline bool is_gas_passive_scalar_on() { return npscal_gas == 0; }
     };
+
     enum GravityMode {
         NoGravity = 0,
         CG        = 1, // conjuguate gradient

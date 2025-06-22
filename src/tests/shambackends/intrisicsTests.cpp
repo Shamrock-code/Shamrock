b31b680bbffe17ab1326d900e91ffc4da47959ea
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-06-22 19:34:29 +0200

    add missing include

--- src/tests/shambackends/intrisicsTests.cpp
+++ src/tests/shambackends/intrisicsTests.cpp
@@ -13,6 +13,7 @@
 #include "shamcomm/logs.hpp"
 #include "shamsys/NodeInstance.hpp"
 #include "shamtest/shamtest.hpp"
+#include <set>
 
 TestStart(Unittest, "shambackends/intrisics::get_sm_id", test_get_sm_id, 1) {
 

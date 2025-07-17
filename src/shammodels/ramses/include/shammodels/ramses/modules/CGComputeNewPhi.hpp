7bc6f3da92598b4131eea602207acc678f365c04
Leodsace Sewanou <leodasce.sewanou@ens-lyon.fr>
2025-07-17 13:35:47 +0200

    [Ramses][Conjugate Gradient] Updates the potential vector for next iteration

---
+++ src/shammodels/ramses/include/shammodels/ramses/modules/CGComputeNewPhi.hpp
@@ -0,0 +1,66 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2024 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file CGComputeNewPhi.hpp
+ * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
+ * @brief
+ *
+ */
+
+#include "shambackends/vec.hpp"
+#include "shammodels/ramses/SolverConfig.hpp"
+#include "shammodels/ramses/solvegraph/OrientedAMRGraphEdge.hpp"
+#include "shamrock/solvergraph/IFieldSpan.hpp"
+#include "shamrock/solvergraph/INode.hpp"
+#include "shamrock/solvergraph/Indexes.hpp"
+
+namespace shammodels::basegodunov::modules {
+
+    template<class T>
+    class NodeComputeNewPhi : public shamrock::solvergraph::INode {
+
+        u32 block_size;
+        T alpha;
+
+        public:
+        NodeComputeNewPhi(u32 block_size, T alpha) : block_size(block_size), alpha(alpha) {}
+
+        struct Edges {
+            const shamrock::solvergraph::Indexes<u32> &sizes;
+            const shamrock::solvergraph::IFieldSpan<T> &spans_phi_p;
+            shamrock::solvergraph::IFieldSpan<T> &spans_phi;
+        };
+
+        inline void set_edges(
+            std::shared_ptr<shamrock::solvergraph::Indexes<u32>> sizes,
+            std::shared_ptr<shamrock::solvergraph::IFieldSpan<T>> spans_phi_p,
+            std::shared_ptr<shamrock::solvergraph::IFieldSpan<T>> spans_phi) {
+            __internal_set_ro_edges({sizes, spans_phi_p});
+            __internal_set_rw_edges({spans_phi});
+        }
+
+        inline Edges get_edges() {
+            return Edges{
+                get_ro_edge<shamrock::solvergraph::Indexes<u32>>(0),
+                get_ro_edge<shamrock::solvergraph::IFieldSpan<T>>(1),
+                get_rw_edge<shamrock::solvergraph::IFieldSpan<T>>(0),
+            };
+        }
+
+        void _impl_evaluate_internal();
+
+        inline virtual std::string _impl_get_label() { return "NodeComputeNewPhi"; };
+
+        virtual std::string _impl_get_tex();
+    };
+
+} // namespace shammodels::basegodunov::modules

6fc5725847f7a5ec8656f27c76bd71f45bb02e00
Leodasce Sewanou <science.ing.guru24.02@gmail.com>
2025-07-18 15:04:36 +0200

    Update src/shammodels/ramses/include/shammodels/ramses/modules/ResidualDot.hpp
    Co-authored-by: David--Cléris Timothée <timothee.davidcleris@proton.me>

--- src/shammodels/ramses/include/shammodels/ramses/modules/ResidualDot.hpp
+++ src/shammodels/ramses/include/shammodels/ramses/modules/ResidualDot.hpp
@@ -35,7 +35,7 @@ using Tscal = shambase::VecComponent<T>;
 
         inline void set_edges(
             std::shared_ptr<shamrock::solvergraph::IFieldRefs<T>> spans_phi_res,
-            std::shared_ptr<shamrock::solvergraph::ScalarEdge<T>> res_ddot) {
+            std::shared_ptr<shamrock::solvergraph::ScalarEdge<Tscal>> res_ddot) {
             __internal_set_ro_edges({spans_phi_res});
             __internal_set_rw_edges({res_ddot});
         }

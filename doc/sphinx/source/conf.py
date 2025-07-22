13459acdd2a695d7bd732b95675d630ff73d9972
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-07-22 16:02:37 +0200

    in sphinx doc

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -72,6 +72,8 @@ sphinx_gallery_conf = {
     "filename_pattern": "/run_",  # Run all examples that start with run_
     "write_computation_times": True,  # write sg_execution_times
     "show_memory": True,
+    #
+    "matplotlib_animations": True,
 }
 
 # -- Options for HTML output -------------------------------------------------

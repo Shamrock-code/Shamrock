486c78ecd464d35a3080f1947969ff1f7a0d7ade
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2025-07-24 15:21:55 +0200

    [Ramses][CG] updated

--- src/shammodels/ramses/src/modules/CGMatVecProd.cpp
+++ src/shammodels/ramses/src/modules/CGMatVecProd.cpp
@@ -10,7 +10,7 @@
 /**
  * @file CGMatVecProd.cpp
  * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
- * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @author Timothée David--Cléris (tim.shamrock@proton.me) --no git blame--
  * @brief Implementation of matrix-vector product [A*p] for the conjugate gradient solver.
  *
  */

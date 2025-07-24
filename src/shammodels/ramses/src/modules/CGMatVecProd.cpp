83c84faba8bb1d58558d699980bbf819346d5697
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2025-07-24 15:01:55 +0200

    [Ramses][CG] updated

--- src/shammodels/ramses/src/modules/CGMatVecProd.cpp
+++ src/shammodels/ramses/src/modules/CGMatVecProd.cpp
@@ -38,92 +38,92 @@ namespace {
 
         public:
         inline static void kernel(Edges &edges, u32 block_size) {
-            edges.cell_neigh_graph.graph.for_each(
-                [&](u64 id, const OrientedAMRGraph &oriented_cell_graph) {
-                    auto &block_level_span = edges.spans_block_level.get_spans().get(id);
-                    auto &block_max_span   = edges.spans_block_max.get_spans().get(id);
-                    auto &block_min_span   = edges.spans_block_min.get_spans().get(id);
-                    auto &phi_p_span       = edges.spans_phi_p.get_spans().get(id);
-                    auto &phi_Ap_span      = edges.spans_phi_Ap.get_spans().get(id);
+            edges.cell_neigh_graph.graph.for_each([&](u64 id,
+                                                      const OrientedAMRGraph &oriented_cell_graph) {
+                auto &block_level_span = edges.spans_block_level.get_spans().get(id);
+                auto &block_max_span   = edges.spans_block_max.get_spans().get(id);
+                auto &block_min_span   = edges.spans_block_min.get_spans().get(id);
+                auto &phi_p_span       = edges.spans_phi_p.get_spans().get(id);
+                auto &phi_Ap_span      = edges.spans_phi_Ap.get_spans().get(id);
 
-                    AMRGraph &graph_neigh_xp
-                        = shambase::get_check_ref(oriented_cell_graph.graph_links[Direction::xp]);
-                    AMRGraph &graph_neigh_xm
-                        = shambase::get_check_ref(oriented_cell_graph.graph_links[Direction::xm]);
-                    AMRGraph &graph_neigh_yp
-                        = shambase::get_check_ref(oriented_cell_graph.graph_links[Direction::yp]);
-                    AMRGraph &graph_neigh_ym
-                        = shambase::get_check_ref(oriented_cell_graph.graph_links[Direction::ym]);
-                    AMRGraph &graph_neigh_zp
-                        = shambase::get_check_ref(oriented_cell_graph.graph_links[Direction::zp]);
-                    AMRGraph &graph_neigh_zm
-                        = shambase::get_check_ref(oriented_cell_graph.graph_links[Direction::zm]);
+                AMRGraph &graph_neigh_xp = shambase::get_check_ref(
+                    oriented_cell_graph.graph_links[shammodels::basegodunov::Direction::xp]);
+                AMRGraph &graph_neigh_xm = shambase::get_check_ref(
+                    oriented_cell_graph.graph_links[shammodels::basegodunov::Direction::xm]);
+                AMRGraph &graph_neigh_yp = shambase::get_check_ref(
+                    oriented_cell_graph.graph_links[shammodels::basegodunov::Direction::yp]);
+                AMRGraph &graph_neigh_ym = shambase::get_check_ref(
+                    oriented_cell_graph.graph_links[shammodels::basegodunov::Direction::ym]);
+                AMRGraph &graph_neigh_zp = shambase::get_check_ref(
+                    oriented_cell_graph.graph_links[shammodels::basegodunov::Direction::zp]);
+                AMRGraph &graph_neigh_zm = shambase::get_check_ref(
+                    oriented_cell_graph.graph_links[shammodels::basegodunov::Direction::zm]);
 
-                    sham::EventList depends_list;
+                sham::EventList depends_list;
 
-                    auto block_level = block_level_span.get_read_access(depends_list);
-                    auto block_min   = block_min_span.get_read_access(depends_list);
-                    auto block_max   = block_max_span.get_read_access(depends_list);
-                    auto phi_p       = phi_p_span.get_read_access(depends_list);
-                    auto phi_Ap      = phi_Ap_span.get_write_access(depends_list);
+                auto block_level = block_level_span.get_read_access(depends_list);
+                auto block_min   = block_min_span.get_read_access(depends_list);
+                auto block_max   = block_max_span.get_read_access(depends_list);
+                auto phi_p       = phi_p_span.get_read_access(depends_list);
+                auto phi_Ap      = phi_Ap_span.get_write_access(depends_list);
 
-                    auto graph_iter_xp = graph_neigh_xp.get_read_access(depends_list);
-                    auto graph_iter_xm = graph_neigh_xm.get_read_access(depends_list);
-                    auto graph_iter_yp = graph_neigh_yp.get_read_access(depends_list);
-                    auto graph_iter_ym = graph_neigh_ym.get_read_access(depends_list);
-                    auto graph_iter_zp = graph_neigh_zp.get_read_access(depends_list);
-                    auto graph_iter_zm = graph_neigh_zm.get_read_access(depends_list);
+                auto graph_iter_xp = graph_neigh_xp.get_read_access(depends_list);
+                auto graph_iter_xm = graph_neigh_xm.get_read_access(depends_list);
+                auto graph_iter_yp = graph_neigh_yp.get_read_access(depends_list);
+                auto graph_iter_ym = graph_neigh_ym.get_read_access(depends_list);
+                auto graph_iter_zp = graph_neigh_zp.get_read_access(depends_list);
+                auto graph_iter_zm = graph_neigh_zm.get_read_access(depends_list);
 
-                    sham::DeviceQueue &q = shamsys::instance::get_compute_scheduler().get_queue();
-                    auto e               = q.submit(depends_list, [&](sycl::handler &cgh) {
-                        u32 cell_count = (edges.sizes.indexes.get(id)) * block_size;
+                sham::DeviceQueue &q = shamsys::instance::get_compute_scheduler().get_queue();
+                auto e               = q.submit(depends_list, [&](sycl::handler &cgh) {
+                    u32 cell_count = (edges.sizes.indexes.get(id)) * block_size;
 
-                        shambase::parallel_for(cgh, cell_count, "compute Ap mat-vec", [=](u64 gid) {
-                            const u32 cell_global_id = (u32) gid;
-                            const u32 block_id       = cell_global_id / block_size;
-                            const u32 cell_loc_id    = cell_global_id % block_size;
+                    shambase::parallel_for(cgh, cell_count, "compute Ap mat-vec", [=](u64 gid) {
+                        const u32 cell_global_id = (u32) gid;
+                        const u32 block_id       = cell_global_id / block_size;
+                        const u32 cell_loc_id    = cell_global_id % block_size;
 
-                            auto Ap_id = laplacian_7pt<Tscal, Tvec, TgridUint>(
-                                cell_global_id,
-                                graph_iter_xp,
-                                graph_iter_xm,
-                                graph_iter_yp,
-                                graph_iter_ym,
-                                graph_iter_zp,
-                                graph_iter_zm,
-                                [=](u32 id) {
-                                    return phi_p[id];
-                                },
+                        auto Ap_id = shammodels::basegodunov::laplacian_7pt<Tscal, Tvec, TgridUint>(
+                            cell_global_id,
+                            graph_iter_xp,
+                            graph_iter_xm,
+                            graph_iter_yp,
+                            graph_iter_ym,
+                            graph_iter_zp,
+                            graph_iter_zm,
+                            [=](u32 id) {
+                                return phi_p[id];
+                            },
 
-                                [=](u32 id) {
-                                    return block_level[id];
-                                },
+                            [=](u32 id) {
+                                return block_level[id];
+                            },
 
-                                [=](u32 id) {
-                                    return block_min[id];
-                                },
+                            [=](u32 id) {
+                                return block_min[id];
+                            },
 
-                                [=](u32 id) {
-                                    return block_max[id];
-                                });
+                            [=](u32 id) {
+                                return block_max[id];
+                            });
 
-                            phi_Ap[cell_global_id] = Ap_id;
-                        });
+                        phi_Ap[cell_global_id] = Ap_id;
                     });
-
-                    block_level_span.complete_event_state(e);
-                    block_max_span.complete_event_state(e);
-                    block_min_span.complete_event_state(e);
-                    phi_p_span.complete_event_state(e);
-                    phi_Ap_span.complete_event_state(e);
-
-                    graph_neigh_xp.complete_event_state(e);
-                    graph_neigh_xm.complete_event_state(e);
-                    graph_neigh_yp.complete_event_state(e);
-                    graph_neigh_ym.complete_event_state(e);
-                    graph_neigh_zp.complete_event_state(e);
-                    graph_neigh_zm.complete_event_state(e);
                 });
+
+                block_level_span.complete_event_state(e);
+                block_max_span.complete_event_state(e);
+                block_min_span.complete_event_state(e);
+                phi_p_span.complete_event_state(e);
+                phi_Ap_span.complete_event_state(e);
+
+                graph_neigh_xp.complete_event_state(e);
+                graph_neigh_xm.complete_event_state(e);
+                graph_neigh_yp.complete_event_state(e);
+                graph_neigh_ym.complete_event_state(e);
+                graph_neigh_zp.complete_event_state(e);
+                graph_neigh_zm.complete_event_state(e);
+            });
         }
     };
 

4edee5672435c444fcb61e89e69b612d998b5ca0
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-07-27 00:19:38 +0200

    test passes

--- src/tests/shammodels/sph/modules/IterateSmoothingLengthDensityTests.cpp
+++ src/tests/shammodels/sph/modules/IterateSmoothingLengthDensityTests.cpp
@@ -37,8 +37,8 @@ TestStart(
 
     constexpr u32 N_particles       = 4 * 4 * 4;
     constexpr Tscal gpart_mass      = 1.0;
-    constexpr Tscal h_evol_max      = 1.1;
-    constexpr Tscal h_evol_iter_max = 1.1;
+    constexpr Tscal h_evol_max      = 1.2;
+    constexpr Tscal h_evol_iter_max = 1.2;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // Parameters of the test
@@ -60,12 +60,181 @@ TestStart(
     std::vector<Tscal> start_h_vec(N_particles, 0.1);
 
     // temporary, to be replaced by the expected h values
-    std::vector<Tscal> expected_h_vec(N_particles, -1);
-
-    u32 expected_iterations = 10;
+    std::vector<Tscal> expected_h_vec_end{
+        1.7039887744498596, 1.4852333966670939, 1.4852333966670939, 1.7039887744498596,
+        1.4852333966670939, 1.3212315623706217, 1.3212315623706217, 1.4852333966670936,
+        1.4852333966670939, 1.3212315623706217, 1.321231562370622,  1.4852333966670936,
+        1.7039887744498596, 1.485233396667094,  1.485233396667094,  1.7039887744498594,
+        1.4852333966670939, 1.3212315623706217, 1.3212315623706217, 1.485233396667094,
+        1.3212315623706217, 1.2024373044416155, 1.2024373044416155, 1.3212315623706217,
+        1.3212315623706217, 1.2024373044416155, 1.2024373044416155, 1.3212315623706217,
+        1.485233396667094,  1.321231562370622,  1.3212315623706217, 1.4852333966670939,
+        1.4852333966670943, 1.3212315623706214, 1.321231562370622,  1.4852333966670943,
+        1.3212315623706217, 1.2024373044416155, 1.2024373044416155, 1.321231562370622,
+        1.3212315623706217, 1.2024373044416155, 1.2024373044416155, 1.321231562370622,
+        1.4852333966670943, 1.321231562370622,  1.321231562370622,  1.4852333966670945,
+        1.7039887744498599, 1.485233396667094,  1.485233396667094,  1.7039887744498599,
+        1.485233396667094,  1.321231562370622,  1.3212315623706217, 1.485233396667094,
+        1.485233396667094,  1.321231562370622,  1.321231562370622,  1.485233396667094,
+        1.7039887744498599, 1.4852333966670945, 1.4852333966670945, 1.70398877444986};
 
     // temporary, to be replaced by the expected h values
-    std::vector<Tscal> expected_eps_vec(N_particles, -1);
+    std::vector<Tscal> expected_eps_vec_end{
+        6.3426814945658865e-12,
+        5.980059576449213e-16,
+        4.48504468233691e-16,
+        6.34224913727246e-12,
+        4.48504468233691e-16,
+        5.041764318586834e-16,
+        5.041764318586834e-16,
+        4.485044682336911e-16,
+        4.48504468233691e-16,
+        5.041764318586834e-16,
+        5.041764318586834e-16,
+        4.485044682336911e-16,
+        6.34224913727246e-12,
+        1.4950148941123038e-16,
+        1.4950148941123038e-16,
+        6.342105018174651e-12,
+        4.48504468233691e-16,
+        5.041764318586834e-16,
+        5.041764318586834e-16,
+        2.990029788224607e-16,
+        5.041764318586834e-16,
+        0.0,
+        0.0,
+        5.041764318586834e-16,
+        3.3611762123912236e-16,
+        0.0,
+        0.0,
+        3.3611762123912236e-16,
+        1.4950148941123038e-16,
+        0.0,
+        3.3611762123912236e-16,
+        2.9900297882246075e-16,
+        1.4950148941123035e-16,
+        5.041764318586835e-16,
+        0.0,
+        1.4950148941123035e-16,
+        3.3611762123912236e-16,
+        0.0,
+        0.0,
+        0.0,
+        3.3611762123912236e-16,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.6805881061956118e-16,
+        0.0,
+        6.342537375468078e-12,
+        1.4950148941123038e-16,
+        1.4950148941123038e-16,
+        6.342537375468078e-12,
+        1.4950148941123038e-16,
+        1.6805881061956118e-16,
+        3.3611762123912236e-16,
+        1.4950148941123038e-16,
+        1.4950148941123038e-16,
+        1.6805881061956123e-16,
+        0.0,
+        1.4950148941123038e-16,
+        6.342537375468078e-12,
+        0.0,
+        0.0,
+        6.3426814945658865e-12};
+
+    std::vector<Tscal> expected_sequence_eps_min{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                                                 -1, -1, -1, -1, -1, -1, -1, -1, 0,  -1,
+                                                 -1, -1, -1, 0,  0,  0,  0,  0};
+    std::vector<Tscal> expected_sequence_eps_max{
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        -1,
+        0.19667779953573308,
+        0.026408786682080153,
+        0.0015808702946168102,
+        5.25907353009218e-06,
+        5.793905946882268e-11,
+        0.18226586196218933,
+        0.03892212639115773,
+        0.0035984720083355017,
+        2.8023453743763244e-05,
+        1.6838139227630172e-09,
+        0.08452448630589766,
+        0.020529594483443016,
+        0.0009261073956681399,
+        1.7604940220352781e-06,
+        6.3426814945658865e-12};
+    std::vector<Tscal> expected_sequence_h_min{
+        0.12,
+        0.144,
+        0.17279999999999998,
+        0.20735999999999996,
+        0.24883199999999994,
+        0.29859839999999993,
+        0.3583180799999999,
+        0.4299816959999999,
+        0.5159780351999999,
+        0.6191736422399998,
+        0.7430083706879997,
+        0.8916100448255997,
+        1.0699320537907195,
+        1.1724846463543073,
+        1.200740253727186,
+        1.202431677528282,
+        1.2024373043796248,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155,
+        1.2024373044416155};
+    std::vector<Tscal> expected_sequence_h_max{
+        0.12,
+        0.144,
+        0.17279999999999998,
+        0.20735999999999996,
+        0.24883199999999994,
+        0.29859839999999993,
+        0.3583180799999999,
+        0.4299816959999999,
+        0.5159780351999999,
+        0.6191736422399998,
+        0.7430083706879997,
+        0.8916100448255997,
+        1.0699320537907195,
+        1.2839184645488635,
+        1.2839184645488635,
+        1.2839184645488635,
+        1.2839184645488635,
+        1.2839184645488635,
+        1.5179329701790327,
+        1.5407021574586361,
+        1.5407021574586361,
+        1.5407021574586361,
+        1.5407021574586361,
+        1.6709292158682156,
+        1.702559206380607,
+        1.7039860620431506,
+        1.7039887744400881,
+        1.70398877444986};
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // The actual test
@@ -150,12 +319,19 @@ TestStart(
     iterate_module.set_edges(
         sizes, neigh_cache, positions_refs, old_h_refs, new_h_refs, eps_h_refs);
 
+    std::vector<Tscal> eps_min_sequence;
+    std::vector<Tscal> eps_max_sequence;
+    std::vector<Tscal> h_min_sequence;
+    std::vector<Tscal> h_max_sequence;
+
     // 9. Run the module
-    for (u32 outer_iter = 0; outer_iter < 10; ++outer_iter) {
+    for (u32 outer_iter = 0; outer_iter < 50; ++outer_iter) {
 
         //  Overwrite the old h values with the new h values
         old_h_field.get_buf().copy_from_stdvec(new_h_field.get_buf().copy_to_stdvec());
+        eps_h_field.get_buf().fill(10000000.);
 
+        Tscal max_eps_h = 10000000.;
         for (u32 inner_iter = 0; inner_iter < 10; ++inner_iter) {
 
             // Run the module
@@ -166,10 +342,20 @@ TestStart(
             std::vector<Tscal> eps_h_result = eps_h_field.get_buf().copy_to_stdvec();
 
             // print h_max and h_min
-            shamcomm::logs::raw_ln(
-                "h_max", *std::max_element(new_h_result.begin(), new_h_result.end()));
-            shamcomm::logs::raw_ln(
-                "h_min", *std::min_element(new_h_result.begin(), new_h_result.end()));
+            // shamcomm::logs::raw_ln(
+            //     "h_max", *std::max_element(new_h_result.begin(), new_h_result.end()));
+            // shamcomm::logs::raw_ln(
+            //     "h_min", *std::min_element(new_h_result.begin(), new_h_result.end()));
+
+            // add results to sequences for validation
+            {
+                eps_min_sequence.push_back(eps_h_field.compute_min());
+                eps_max_sequence.push_back(eps_h_field.compute_max());
+                h_min_sequence.push_back(
+                    *std::min_element(new_h_result.begin(), new_h_result.end()));
+                h_max_sequence.push_back(
+                    *std::max_element(new_h_result.begin(), new_h_result.end()));
+            }
 
             // Verify results
             u32 eps_expected_range_offenses = 0;
@@ -187,6 +373,34 @@ TestStart(
                 }
             }
             REQUIRE_EQUAL(new_h_expected_range_offenses, 0);
+
+            // get max eps_h
+            max_eps_h = eps_h_field.compute_max();
+            // shamcomm::logs::raw_ln("max_eps_h", max_eps_h);
+
+            // either converged or need a new outer iteration (early break)
+            if (max_eps_h < 1e-6) {
+                break;
+            }
+        }
+
+        // get min eps_h
+        Tscal min_eps_h = eps_h_field.compute_min();
+        // shamcomm::logs::raw_ln("min_eps_h", min_eps_h);
+
+        // need a new outer iteration
+        if (min_eps_h == -1) {
+            continue;
+        }
+
+        // is converged, we can break
+        if (max_eps_h < 1e-6) {
+            break;
+        }
+
+        // if last outer iteration, we need to crash, since we did not converge
+        if (outer_iter == 50) {
+            REQUIRE_NAMED("The h iterator is not converged after 50 iterations", false);
         }
     }
 
@@ -195,7 +409,27 @@ TestStart(
         std::vector<Tscal> new_h_result = new_h_field.get_buf().copy_to_stdvec();
         std::vector<Tscal> eps_h_result = eps_h_field.get_buf().copy_to_stdvec();
 
-        REQUIRE_EQUAL(new_h_result, expected_h_vec);
-        REQUIRE_EQUAL(eps_h_result, expected_eps_vec);
+        constexpr Tscal tol = 1e-6;
+        auto almost_equal   = [tol](const std::vector<Tscal> &a, const std::vector<Tscal> &b) {
+            // check sizes
+            if (a.size() != b.size()) {
+                return false;
+            }
+
+            // check values
+            for (u32 i = 0; i < a.size(); ++i) {
+                if (std::abs(a[i] - b[i]) > tol) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        REQUIRE_EQUAL_CUSTOM_COMP(new_h_result, expected_h_vec_end, almost_equal);
+        REQUIRE_EQUAL_CUSTOM_COMP(eps_h_result, expected_eps_vec_end, almost_equal);
+        REQUIRE_EQUAL_CUSTOM_COMP(eps_min_sequence, expected_sequence_eps_min, almost_equal);
+        REQUIRE_EQUAL_CUSTOM_COMP(eps_max_sequence, expected_sequence_eps_max, almost_equal);
+        REQUIRE_EQUAL_CUSTOM_COMP(h_min_sequence, expected_sequence_h_min, almost_equal);
+        REQUIRE_EQUAL_CUSTOM_COMP(h_max_sequence, expected_sequence_h_max, almost_equal);
     }
 }

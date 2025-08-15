19913449048b5aa983de028632e6d657ed2a9456
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2025-08-15 13:28:23 +0200

    [Ramses][Poisson] Nodes for Jacobi-Preconditioner and overwrite (Y = X)

---
+++ src/shammodels/ramses/include/shammodels/ramses/modules/NodeJacobiPreconditioner.hpp
@@ -0,0 +1,69 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file NodeJacobiPreconditioner.hpp
+ * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
+ * @brief
+ *
+ */
+
+#include "shambackends/vec.hpp"
+#include "shammodels/ramses/SolverConfig.hpp"
+#include "shamrock/solvergraph/IFieldRefs.hpp"
+#include "shamrock/solvergraph/IFieldSpan.hpp"
+#include "shamrock/solvergraph/INode.hpp"
+#include "shamrock/solvergraph/Indexes.hpp"
+#include "shamrock/solvergraph/ScalarEdge.hpp"
+
+namespace shammodels::basegodunov::modules {
+
+    template<class T>
+    class NodeJacobiPreconditioner : public shamrock::solvergraph::INode {
+
+        u32 block_size;
+
+        public:
+        NodeJacobiPreconditioner(u32 block_size) : block_size(block_size) {}
+
+        struct Edges {
+            const shamrock::solvergraph::Indexes<u32> &sizes;
+            const shamrock::solvergraph::IFieldSpan<T> &spans_block_cell_sizes;
+            const shamrock::solvergraph::IFieldSpan<T> &spans_x;
+            shamrock::solvergraph::IFieldRefs<T> &spans_y;
+        };
+
+        inline void set_edges(
+            std::shared_ptr<shamrock::solvergraph::Indexes<u32>> sizes,
+            std::shared_ptr<shamrock::solvergraph::IFieldSpan<T>> spans_block_cell_sizes,
+            std::shared_ptr<shamrock::solvergraph::IFieldSpan<T>> spans_x,
+            std::shared_ptr<shamrock::solvergraph::IFieldRefs<T>> spans_y) {
+            __internal_set_ro_edges({sizes, spans_x});
+            __internal_set_rw_edges({spans_y});
+        }
+
+        inline Edges get_edges() {
+            return Edges{
+                get_ro_edge<shamrock::solvergraph::Indexes<u32>>(0),
+                get_ro_edge<shamrock::solvergraph::IFieldSpan<T>>(1),
+                get_ro_edge<shamrock::solvergraph::IFieldSpan<T>>(2),
+                get_rw_edge<shamrock::solvergraph::IFieldRefs<T>>(0),
+            };
+        }
+
+        void _impl_evaluate_internal();
+
+        inline virtual std::string _impl_get_label() { return "NodeJacobiPreconditioner"; };
+
+        virtual std::string _impl_get_tex();
+    };
+
+} // namespace shammodels::basegodunov::modules

af44026da2184e84786f0103a04f71442a43a37f
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-08-29 15:46:53 +0200

    [Algs] add decoupled scan in place to primitives

---
+++ src/shamalgs/include/shamalgs/primitives/scan_exclusive_sum_in_place.hpp
@@ -0,0 +1,26 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file scan_exclusive_sum_in_place.hpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief
+ */
+
+#include "shambase/aliases_int.hpp"
+#include "shambackends/DeviceBuffer.hpp"
+
+namespace shamalgs::primitives {
+
+    template<class T>
+    void scan_exclusive_sum_in_place(sham::DeviceBuffer<T> &buf1, u32 len);
+
+}

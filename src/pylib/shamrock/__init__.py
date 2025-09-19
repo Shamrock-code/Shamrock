919b17ffd27fef0015e3bd2b37f20501cbb6f080
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-09-19 15:17:10 +0200

    it somewhat works

--- src/pylib/shamrock/__init__.py
+++ src/pylib/shamrock/__init__.py
@@ -1,1 +1,8 @@
-from pyshamrock import *
+try:
+    from .pyshamrock import *
+except ImportError:
+    try:
+        from shamrock import *
+    except ImportError:
+        from pyshamrock import *
+    

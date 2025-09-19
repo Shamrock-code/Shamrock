193fad6bff9023aeae29d0ebb7eee3c17efab842
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-09-19 13:04:58 +0200

    [Draft] attempt at adding a native python lib for utilities

--- src/shambindings/src/run_ipython.py
+++ src/shambindings/src/run_ipython.py
@@ -4,14 +4,6 @@ import sys
 from IPython import start_ipython
 from traitlets.config.loader import Config
 
-# here the signal interup for sigint is None
-# this make ipython freaks out for weird reasons
-# registering the handler fix it ...
-# i swear python c api is horrible to works with
-import shamrock.sys
-
-signal.signal(signal.SIGINT, shamrock.sys.signal_handler)
-
 c = Config()
 
 banner = "SHAMROCK Ipython terminal\n" + "Python %s\n" % sys.version.split("\n")[0]

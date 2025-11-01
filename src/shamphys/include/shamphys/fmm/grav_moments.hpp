3c4b4ceb1d88deab33d2cd399723d01263e4d1ac
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-11-01 13:14:19 +0100

    more const

--- src/shamphys/include/shamphys/fmm/grav_moments.hpp
+++ src/shamphys/include/shamphys/fmm/grav_moments.hpp
@@ -23,26 +23,26 @@ namespace shamphys {
     /// Contraction of the green function derivatives (D_n) and the multipole moments (Q_n)
     template<class T, u32 low_order, u32 high_order>
     inline shammath::SymTensorCollection<T, low_order, high_order> get_M_mat(
-        shammath::SymTensorCollection<T, low_order, high_order> &D,
-        shammath::SymTensorCollection<T, low_order, high_order> &Q) {
+        const shammath::SymTensorCollection<T, low_order, high_order> &D,
+        const shammath::SymTensorCollection<T, low_order, high_order> &Q) {
 
         using namespace shammath;
 
         if constexpr (low_order == 0 && high_order == 5) {
 
-            T &TD0                = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
-            SymTensor3d_3<T> &TD3 = D.t3;
-            SymTensor3d_4<T> &TD4 = D.t4;
-            SymTensor3d_5<T> &TD5 = D.t5;
+            const T &TD0                = D.t0;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_3<T> &TD3 = D.t3;
+            const SymTensor3d_4<T> &TD4 = D.t4;
+            const SymTensor3d_5<T> &TD5 = D.t5;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
-            SymTensor3d_3<T> &TQ3 = Q.t3;
-            SymTensor3d_4<T> &TQ4 = Q.t4;
-            SymTensor3d_5<T> &TQ5 = Q.t5;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
+            const SymTensor3d_3<T> &TQ3 = Q.t3;
+            const SymTensor3d_4<T> &TQ4 = Q.t4;
+            const SymTensor3d_5<T> &TQ5 = Q.t5;
 
             auto M_0 = (TD0 * TQ0) + (TD1 * TQ1) + ((TD2 * TQ2)) * T(1 / 2.)
                        + ((TD3 * TQ3)) * T(1 / 6.) + ((TD4 * TQ4)) * T(1 / 24.)
@@ -58,17 +58,17 @@ namespace shamphys {
             return SymTensorCollection<T, 0, 5>{M_0, M_1, M_2, M_3, M_4, M_5};
         } else if constexpr (low_order == 0 && high_order == 4) {
 
-            T &TD0                = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
-            SymTensor3d_3<T> &TD3 = D.t3;
-            SymTensor3d_4<T> &TD4 = D.t4;
+            const T &TD0                = D.t0;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_3<T> &TD3 = D.t3;
+            const SymTensor3d_4<T> &TD4 = D.t4;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
-            SymTensor3d_3<T> &TQ3 = Q.t3;
-            SymTensor3d_4<T> &TQ4 = Q.t4;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
+            const SymTensor3d_3<T> &TQ3 = Q.t3;
+            const SymTensor3d_4<T> &TQ4 = Q.t4;
 
             auto M_0 = (TD0 * TQ0) + (TD1 * TQ1) + ((TD2 * TQ2)) * T(1 / 2.)
                        + ((TD3 * TQ3)) * T(1 / 6.) + ((TD4 * TQ4)) * T(1 / 24.);
@@ -81,15 +81,15 @@ namespace shamphys {
             return SymTensorCollection<T, 0, 4>{M_0, M_1, M_2, M_3, M_4};
         } else if constexpr (low_order == 0 && high_order == 3) {
 
-            T &TD0                = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
-            SymTensor3d_3<T> &TD3 = D.t3;
+            const T &TD0                = D.t0;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_3<T> &TD3 = D.t3;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
-            SymTensor3d_3<T> &TQ3 = Q.t3;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
+            const SymTensor3d_3<T> &TQ3 = Q.t3;
 
             auto M_0
                 = (TD0 * TQ0) + (TD1 * TQ1) + ((TD2 * TQ2)) * T(1 / 2.) + ((TD3 * TQ3)) * T(1 / 6.);
@@ -100,13 +100,13 @@ namespace shamphys {
             return SymTensorCollection<T, 0, 3>{M_0, M_1, M_2, M_3};
         } else if constexpr (low_order == 0 && high_order == 2) {
 
-            T &TD0                = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
+            const T &TD0                = D.t0;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
 
             auto M_0 = (TD0 * TQ0) + (TD1 * TQ1) + ((TD2 * TQ2)) * T(1 / 2.);
             auto M_1 = T(-1.) * (TD1 * TQ0) - (TD2 * TQ1);
@@ -115,11 +115,11 @@ namespace shamphys {
             return SymTensorCollection<T, 0, 2>{M_0, M_1, M_2};
         } else if constexpr (low_order == 0 && high_order == 1) {
 
-            T &TD0                = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
+            const T &TD0                = D.t0;
+            const SymTensor3d_1<T> &TD1 = D.t1;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
 
             auto M_0 = (TD0 * TQ0) + (TD1 * TQ1);
             auto M_1 = T(-1.) * (TD1 * TQ0);
@@ -127,9 +127,9 @@ namespace shamphys {
             return SymTensorCollection<T, 0, 1>{M_0, M_1};
         } else if constexpr (low_order == 0 && high_order == 0) {
 
-            T &TD0 = D.t0;
+            const T &TD0 = D.t0;
 
-            T &TQ0 = Q.t0;
+            const T &TQ0 = Q.t0;
 
             auto M_0 = TD0 * TQ0;
 
@@ -143,24 +143,24 @@ namespace shamphys {
     /// This is also the nabla of the M_mat
     template<class T, u32 high_order>
     inline shammath::SymTensorCollection<T, 1, high_order + 1> get_dM_mat(
-        shammath::SymTensorCollection<T, 1, high_order + 1> &D,
-        shammath::SymTensorCollection<T, 0, high_order> &Q) {
+        const shammath::SymTensorCollection<T, 1, high_order + 1> &D,
+        const shammath::SymTensorCollection<T, 0, high_order> &Q) {
 
         using namespace shammath;
 
         if constexpr (high_order == 4) {
             // T & TD0 = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
-            SymTensor3d_3<T> &TD3 = D.t3;
-            SymTensor3d_4<T> &TD4 = D.t4;
-            SymTensor3d_5<T> &TD5 = D.t5;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_3<T> &TD3 = D.t3;
+            const SymTensor3d_4<T> &TD4 = D.t4;
+            const SymTensor3d_5<T> &TD5 = D.t5;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
-            SymTensor3d_3<T> &TQ3 = Q.t3;
-            SymTensor3d_4<T> &TQ4 = Q.t4;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
+            const SymTensor3d_3<T> &TQ3 = Q.t3;
+            const SymTensor3d_4<T> &TQ4 = Q.t4;
             // SymTensor3d_5<T> & TQ5 = Q.t5;
 
             constexpr T _1i2  = T(1. / 2.);
@@ -178,15 +178,15 @@ namespace shamphys {
         } else if constexpr (high_order == 3) {
 
             // T & TD0 = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
-            SymTensor3d_3<T> &TD3 = D.t3;
-            SymTensor3d_4<T> &TD4 = D.t4;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_3<T> &TD3 = D.t3;
+            const SymTensor3d_4<T> &TD4 = D.t4;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
-            SymTensor3d_3<T> &TQ3 = Q.t3;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
+            const SymTensor3d_3<T> &TQ3 = Q.t3;
 
             auto M_1 = TD1 * TQ0 + TD2 * TQ1 + (TD3 * TQ2) * T(1. / 2.) + (TD4 * TQ3) * T(1. / 6.);
             auto M_2 = (T(-1.) * TD2 * TQ0) - TD3 * TQ1 - (TD4 * TQ2) * T(1. / 2.);
@@ -197,13 +197,13 @@ namespace shamphys {
         } else if constexpr (high_order == 2) {
 
             // T & TD0 = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
-            SymTensor3d_3<T> &TD3 = D.t3;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_3<T> &TD3 = D.t3;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
-            SymTensor3d_2<T> &TQ2 = Q.t2;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
+            const SymTensor3d_2<T> &TQ2 = Q.t2;
 
             auto M_1 = TD1 * TQ0 + TD2 * TQ1 + (TD3 * TQ2) * (1. / 2.);
             auto M_2 = (T(-1.) * TD2 * TQ0) - TD3 * TQ1;
@@ -213,11 +213,11 @@ namespace shamphys {
         } else if constexpr (high_order == 1) {
 
             // T & TD0 = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
-            SymTensor3d_2<T> &TD2 = D.t2;
+            const SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_2<T> &TD2 = D.t2;
 
-            T &TQ0                = Q.t0;
-            SymTensor3d_1<T> &TQ1 = Q.t1;
+            const T &TQ0                = Q.t0;
+            const SymTensor3d_1<T> &TQ1 = Q.t1;
 
             auto M_1 = TD1 * TQ0 + TD2 * TQ1;
             auto M_2 = T(-1.) * TD2 * TQ0;
@@ -225,9 +225,9 @@ namespace shamphys {
             return SymTensorCollection<T, 1, 2>{M_1, M_2};
         } else if constexpr (high_order == 0) {
             // T & TD0 = D.t0;
-            SymTensor3d_1<T> &TD1 = D.t1;
+            const SymTensor3d_1<T> &TD1 = D.t1;
 
-            T &TQ0 = Q.t0;
+            const T &TQ0 = Q.t0;
 
             auto M_1 = TD1 * TQ0;
 

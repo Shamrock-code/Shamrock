adac600ca24defdf83e52db0924eed380f69f360
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-11-23 19:10:22 +0100

    Apply suggestion from @tdavidcl

--- src/shamrock/include/shamrock/solvergraph/ExtractGhostField.hpp
+++ src/shamrock/include/shamrock/solvergraph/ExtractGhostField.hpp
@@ -55,8 +55,8 @@ namespace shamrock::solvergraph {
 
         void _impl_evaluate_internal();
 
-        inline virtual std::string _impl_get_label() { return "ExtractGhostField"; };
+        inline virtual std::string _impl_get_label() const { return "ExtractGhostField"; };
 
-        virtual std::string _impl_get_tex() { return "TODO"; };
+        virtual std::string _impl_get_tex() const { return "TODO"; };
     };
 } // namespace shamrock::solvergraph

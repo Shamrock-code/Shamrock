2b0804a3cb468e188ce6315ea0eed96545c2c5d4
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-11-23 19:57:28 +0100

    Update src/shamrock/include/shamrock/solvergraph/ExtractGhostField.hpp

--- src/shamrock/include/shamrock/solvergraph/ExtractGhostField.hpp
+++ src/shamrock/include/shamrock/solvergraph/ExtractGhostField.hpp
@@ -12,7 +12,7 @@
 /**
  * @file ExtractGhostField.hpp
  * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
- * @author Timothée David--Cléris (tim.shamrock@proton.me) --no git blame--
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @brief  This module allows to extract ghosts for a generic field such as density, velocity, etc
  *
  */

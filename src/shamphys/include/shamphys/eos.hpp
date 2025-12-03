13f77fa7cdd415f2c9e0a6535d4acabcb8cf4c07
David Fang <david.fang@ikmail.com>
2025-12-04 00:36:52 +0100

    authorship

--- src/shamphys/include/shamphys/eos.hpp
+++ src/shamphys/include/shamphys/eos.hpp
@@ -11,6 +11,7 @@
 
 /**
  * @file eos.hpp
+ * @author David Fang (david.fang@ikmail.com)
  * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @author Yona Lapeyre (yona.lapeyre@ens-lyon.fr)
  * @brief

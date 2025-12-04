6e5c81a4f418658b523972f60b91030d656b5d2c
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2025-12-04 15:16:17 +0100

    [Ramses][AMR][RefinementHandler] updates

--- src/shammodels/common/include/shammodels/common/amr/NeighGraph.hpp
+++ src/shammodels/common/include/shammodels/common/amr/NeighGraph.hpp
@@ -11,8 +11,8 @@
 
 /**
  * @file NeighGraph.hpp
- * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @brief
  *
  */

0918752c96e79593d6feb1b4db15764b2d965260
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2025-12-05 16:42:23 +0100

    [Ramses][AMR][RefinementHandler] updates

--- src/shammodels/ramses/src/modules/AMRGridRefinementHandler.cpp
+++ src/shammodels/ramses/src/modules/AMRGridRefinementHandler.cpp
@@ -10,6 +10,7 @@
 /**
  * @file AMRGridRefinementHandler.cpp
  * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
+ * @author Sewanou Leodasce (lsewanou@pxe.cbp.ens-lyon.fr)
  * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @brief
  *

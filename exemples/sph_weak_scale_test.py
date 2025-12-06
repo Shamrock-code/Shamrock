e0814cbe3f131f3ed8dc680195f263191cc037e4
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-12-06 15:46:10 +0100

    Update exemples/sph_weak_scale_test.py

--- exemples/sph_weak_scale_test.py
+++ exemples/sph_weak_scale_test.py
@@ -67,7 +67,7 @@ setup = model.get_setup()
 gen = setup.make_generator_lattice_hcp(dr, bmin, bmax)
 
 # On aurora /2 was correct to avoid out of memory
-setup.apply_setup(gen, insert_step=int(scheduler_split_val / 4))
+setup.apply_setup(gen, insert_step=int(scheduler_split_val / 2))
 
 
 xc, yc, zc = model.get_closest_part_to((0, 0, 0))

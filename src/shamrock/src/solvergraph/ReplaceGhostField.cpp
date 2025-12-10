a6bd706a2122700414991c9593d1dd60fda7908f
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-12-10 11:25:07 +0100

    Update src/shamrock/src/solvergraph/ReplaceGhostField.cpp

--- src/shamrock/src/solvergraph/ReplaceGhostField.cpp
+++ src/shamrock/src/solvergraph/ReplaceGhostField.cpp
@@ -10,7 +10,7 @@
 /**
  * @file ReplaceGhostField.cpp
  * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
- * @author Timothée David--Cléris (tim.shamrock@proton.me) --no git blame--
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @brief   This module allows replacing ghost values for a generic field with updated values.
  *
  */

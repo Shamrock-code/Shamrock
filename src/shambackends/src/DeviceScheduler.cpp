1e5e75b007dfe3a7471326a6489036310f734d97
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-12-20 15:43:34 +0100

    [Backends] remove use of sycl::buffer within queue test

--- src/shambackends/src/DeviceScheduler.cpp
+++ src/shambackends/src/DeviceScheduler.cpp
@@ -16,6 +16,8 @@
 #include "shambackends/DeviceScheduler.hpp"
 #include "shambase/exception.hpp"
 #include "shambase/string.hpp"
+#include "shambackends/DeviceBuffer.hpp"
+#include "shambackends/kernel_call.hpp"
 #include "shamcomm/logs.hpp"
 
 namespace sham {
@@ -39,12 +41,52 @@ namespace sham {
         }
     }
 
-    void DeviceScheduler::test() {
-        for (auto &q : queues) {
-            q->test();
-        }
-    }
-
     bool DeviceScheduler::use_direct_comm() { return ctx->use_direct_comm(); }
 
+    void test_device_scheduler(sham::DeviceScheduler_ptr dev_sched) {
+        for (auto &q : dev_sched->queues) {
+
+            auto test_kernel = [&](sham::DeviceQueue &q) {
+                sham::DeviceBuffer<u32> b(10, dev_sched);
+
+                sham::kernel_call(q, sham::MultiRef{}, sham::MultiRef{b}, 10, [](u32 i, u32 *b) {
+                    b[i] = i;
+                });
+
+                {
+                    std::vector<u32> acc = b.copy_to_stdvec();
+                    if (acc[9] != 9) {
+                        throw shambase::make_except_with_loc<std::runtime_error>(
+                            "The chosen SYCL queue cannot execute a basic kernel");
+                    }
+                }
+            };
+
+            std::exception_ptr eptr;
+            try {
+                sham::DeviceQueue &qref     = shambase::get_check_ref(q);
+                sham::DeviceContext &ctxref = shambase::get_check_ref(qref.ctx);
+                sham::Device &deviceref     = shambase::get_check_ref(ctxref.device);
+                std::string device_name     = deviceref.dev.get_info<sycl::info::device::name>();
+
+                logger::debug_ln("Backends", "[Queue testing] name = ", device_name);
+                test_kernel(qref);
+                logger::debug_ln(
+                    "Backends", "[Queue testing] name = ", device_name, " -> working !");
+
+            } catch (...) {
+                eptr = std::current_exception(); // capture
+            }
+
+            if (eptr) {
+                sham::DeviceQueue &qref     = shambase::get_check_ref(q);
+                sham::DeviceContext &ctxref = shambase::get_check_ref(qref.ctx);
+                sham::Device &deviceref     = shambase::get_check_ref(ctxref.device);
+                std::string device_name     = deviceref.dev.get_info<sycl::info::device::name>();
+                logger::err_ln(
+                    "Backends", "[Queue testing] name = ", device_name, " -> not working !");
+                std::rethrow_exception(eptr);
+            }
+        }
+    }
 } // namespace sham

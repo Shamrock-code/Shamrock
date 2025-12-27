637d2d7f5b807b463211b6ebc2f64f0b4fd10702
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-12-27 21:29:41 +0100

    [Doc] document some random stuff

--- src/shambackends/include/shambackends/group_op.hpp
+++ src/shambackends/include/shambackends/group_op.hpp
@@ -22,7 +22,17 @@
 
 namespace sham {
 
-#ifdef SYCL2020_FEATURE_GROUP_REDUCTION
+    /**
+     * @brief Apply function to each component of a vector
+     *
+     * @tparam T Vector type
+     * @tparam Func Function type
+     * @tparam Args Variadic argument types
+     * @param in Input vector
+     * @param f Function to apply to each component
+     * @param args Additional arguments to forward to the function
+     * @return T Vector with function applied to each component
+     */
     template<class T, class Func, class... Args>
     inline T map_vector(const T &in, Func &&f, Args... args) {
 
@@ -81,49 +91,73 @@ namespace sham {
     // This messes up the reduction and every component will be reduced by the same value
     // I have no feaking idea why
 
+    /**
+     * @brief Sum reduction across work-group
+     *
+     * @tparam T Value type to reduce
+     * @param g SYCL work-group
+     * @param v Value to reduce
+     * @return T Sum of values across all work-items in the group
+     */
     template<class T>
     inline T sum_over_group(const sycl::group<1> &g, const T &v) {
-    #ifdef SYCL_COMP_INTEL_LLVM
+#ifdef SYCL_COMP_INTEL_LLVM
         return sycl::reduce_over_group(g, v, sycl::plus<>());
-    #endif
-    #ifdef SYCL_COMP_ACPP
+#endif
+#ifdef SYCL_COMP_ACPP
         return map_vector(
             v,
             [](auto component, const sycl::group<1> &g) {
                 return sycl::reduce_over_group(g, component, sycl::plus<decltype(component)>{});
             },
             g);
-    #endif
+#endif
     }
 
+    /**
+     * @brief Minimum reduction across work-group
+     *
+     * @tparam T Value type to reduce
+     * @param g SYCL work-group
+     * @param v Value to reduce
+     * @return T Minimum value across all work-items in the group
+     */
     template<class T>
     inline T min_over_group(const sycl::group<1> &g, const T &v) {
-    #ifdef SYCL_COMP_INTEL_LLVM
+#ifdef SYCL_COMP_INTEL_LLVM
         return sycl::reduce_over_group(g, v, sycl::minimum<>());
-    #endif
-    #ifdef SYCL_COMP_ACPP
+#endif
+#ifdef SYCL_COMP_ACPP
         return map_vector(
             v,
             [](auto component, const sycl::group<1> &g) {
                 return sycl::reduce_over_group(g, component, sycl::minimum<decltype(component)>{});
             },
             g);
-    #endif
+#endif
     }
 
+    /**
+     * @brief Maximum reduction across work-group
+     *
+     * @tparam T Value type to reduce
+     * @param g SYCL work-group
+     * @param v Value to reduce
+     * @return T Maximum value across all work-items in the group
+     */
     template<class T>
     inline T max_over_group(const sycl::group<1> &g, const T &v) {
-    #ifdef SYCL_COMP_INTEL_LLVM
+#ifdef SYCL_COMP_INTEL_LLVM
         return sycl::reduce_over_group(g, v, sycl::maximum<>());
-    #endif
-    #ifdef SYCL_COMP_ACPP
+#endif
+#ifdef SYCL_COMP_ACPP
         return map_vector(
             v,
             [](auto component, const sycl::group<1> &g) {
                 return sycl::reduce_over_group(g, component, sycl::maximum<decltype(component)>{});
             },
             g);
-    #endif
+#endif
     }
 #endif
 

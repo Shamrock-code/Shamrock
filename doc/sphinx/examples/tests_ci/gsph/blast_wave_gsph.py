ec620e5729cad1224f35ed0c4e006f81fe03e36d
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-12-31 18:08:25 +0100

    Update doc/sphinx/examples/tests_ci/gsph/blast_wave_gsph.py

--- doc/sphinx/examples/tests_ci/gsph/blast_wave_gsph.py
+++ doc/sphinx/examples/tests_ci/gsph/blast_wave_gsph.py
@@ -62,7 +62,7 @@ sod = shamrock.phys.SodTube(gamma=gamma, rho_1=rho_L, P_1=P_L, rho_5=rho_R, P_5=
 
 data = ctx.collect_data()
 
-def compute_L2_errors(ctx, sod, t, x_min, x_max):
+def compute_L2_errors(data, sod, t, x_min, x_max):
     """Compute L2 errors using ctx.collect_data() (no pyvista dependency)."""
     points = np.array(data["xyz"])
     velocities = np.array(data["vxyz"])

76e465feed1ebf6a2feb4b343ec8cc79d6e6e573
Guo <guo.yansong@optimind.tech>
2026-01-09 05:43:27 +0900

    Fix pre-commit: license headers and accents

--- src/shammodels/gsph/src/physics/newtonian/NewtonianMode.cpp
+++ src/shammodels/gsph/src/physics/newtonian/NewtonianMode.cpp
@@ -1,7 +1,7 @@
 // -------------------------------------------------------//
 //
 // SHAMROCK code for hydrodynamics
-// Copyright (c) 2021-2025 Timothee David--Cleris <tim.shamrock@proton.me>
+// Copyright (c) 2021-2025 Timothée David--Cléris <tim.shamrock@proton.me>
 // SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
 // Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
 //
@@ -10,7 +10,7 @@
 /**
  * @file NewtonianMode.cpp
  * @author Guo Yansong (guo.yansong.ngy@gmail.com)
- * @author Timothee David--Cleris (tim.shamrock@proton.me) --no git blame--
+ * @author Timothée David--Cléris (tim.shamrock@proton.me) --no git blame--
  * @brief Implementation of Newtonian physics mode for GSPH
  *
  * Owns the complete timestep sequence:

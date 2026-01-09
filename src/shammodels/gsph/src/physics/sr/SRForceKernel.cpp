40c7999f6307bfd6ceb58d7f18662fff8772b6ac
Guo <guo.yansong@optimind.tech>
2026-01-09 19:54:30 +0900

    Update author headers with git blame annotations
    Run buildbot/update_authors.py to add --no git blame-- annotations
    to author headers as required by CI checks.

--- src/shammodels/gsph/src/physics/sr/SRForceKernel.cpp
+++ src/shammodels/gsph/src/physics/sr/SRForceKernel.cpp
@@ -10,7 +10,7 @@
 /**
  * @file SRForceKernel.cpp
  * @author Guo Yansong (guo.yansong.ngy@gmail.com)
- * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @author Timothée David--Cléris (tim.shamrock@proton.me) --no git blame--
  * @author Yona Lapeyre (yona.lapeyre@ens-lyon.fr) --no git blame--
  * @brief Implementation of Special Relativistic GSPH force kernel
  *

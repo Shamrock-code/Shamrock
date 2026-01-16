343410a0e22f09fb0fd7e638b20810c2ba499719
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-01-16 20:04:03 +0100

    attempt

--- src/pylib/shamrock/sys/__init__.py
+++ src/pylib/shamrock/sys/__init__.py
@@ -1,6 +1,6 @@
 try:
     # try to import from the global namespace (works if embedded python interpreter is used)
-    from pyshamrock.math import *
+    from pyshamrock.sys import *
 except ImportError:
     # then it is a library mode, we import from the local namespace
-    from ..pyshamrock.math import *
+    from ..pyshamrock.sys import *

32c02a24121df93bfa40e92ef996670d5f7bbce6
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-01-20 09:02:55 +0100

    Update src/shammodels/ramses/src/Solver.cpp

--- src/shammodels/ramses/src/Solver.cpp
+++ src/shammodels/ramses/src/Solver.cpp
@@ -926,7 +926,7 @@ void shammodels::basegodunov::Solver<Tvec, TgridVec>::init_solver_graph() {
         solver_sequence.push_back(std::make_shared<decltype(node)>(std::move(node)));
     }
 
-    { // compute level0 sizes in patch
+    if(false) { // compute level0 sizes in patch (to be enabled later when needed)
         modules::ComputeLevel0CellSize<Tvec, TgridVec> node_level0_sizes{};
         node_level0_sizes.set_edges(
             graph.get_edge_ptr<ScalarsEdge<shammath::AABB<TgridVec>>>("global_patch_boxes"),

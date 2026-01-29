d0875d3afb498ea513695bbed952a416a88bd54d
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-01-29 12:00:25 +0100

    update

--- src/pylib/shamrock/utils/dump/__init__.py
+++ src/pylib/shamrock/utils/dump/__init__.py
@@ -4,9 +4,9 @@ import os
 import shamrock.sys
 
 
-def purge_old_dumps(dump_prefix, keep_first=1, keep_last=3):
+def purge_old_dumps(dump_prefix, keep_first=1, keep_last=3, ext=".sham"):
     if shamrock.sys.world_rank() == 0:
-        res = glob.glob(dump_prefix + "*.sham")
+        res = glob.glob(dump_prefix + "*" + ext)
         res.sort()
 
         # The list of dumps to remove (keep the first and last 3 dumps)
@@ -16,14 +16,14 @@ def purge_old_dumps(dump_prefix, keep_first=1, keep_last=3):
             os.remove(f)
 
 
-def get_last_dump(dump_prefix):
-    res = glob.glob(dump_prefix + "*.sham")
+def get_last_dump(dump_prefix, ext=".sham"):
+    res = glob.glob(dump_prefix + "*" + ext)
 
     num_max = -1
 
     for f in res:
         try:
-            dump_num = int(f[len(dump_prefix) : -5])
+            dump_num = int(f[len(dump_prefix) : -len(ext)])
             if dump_num > num_max:
                 num_max = dump_num
         except ValueError:
@@ -36,20 +36,20 @@ def get_last_dump(dump_prefix):
 
 
 class ShamrockDumpHandleHelper:
-    def __init__(self, model, dump_prefix):
+    def __init__(self, model, dump_prefix, ext=".sham"):
         self.model = model
         self.dump_prefix = dump_prefix
-
+        self.ext = ext
         os.makedirs(os.path.dirname(self.dump_prefix), exist_ok=True)
 
     def get_dump_name(self, idump):
-        return self.dump_prefix + f"{idump:07}" + ".sham"
+        return self.dump_prefix + f"{idump:07}" + self.ext
 
     def get_last_dump(self):
-        return get_last_dump(self.dump_prefix)
+        return get_last_dump(self.dump_prefix, self.ext)
 
     def purge_old_dumps(self, keep_first=1, keep_last=3):
-        purge_old_dumps(self.dump_prefix, keep_first, keep_last)
+        purge_old_dumps(self.dump_prefix, keep_first, keep_last, self.ext)
 
     def load_dump(self, idump):
         dump_name = self.get_dump_name(idump)

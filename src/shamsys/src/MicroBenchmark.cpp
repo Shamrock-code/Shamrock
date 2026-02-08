b67e29baf21ccfb8062e53178fa69a0525fa7f30
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-02-08 15:35:49 +0100

    [Sys] limit bandwidth test in microbenchmark to max alloc size

--- src/shamsys/src/MicroBenchmark.cpp
+++ src/shamsys/src/MicroBenchmark.cpp
@@ -222,7 +222,7 @@ void shamsys::microbench::saxpy() {
         auto &dev       = shambase::get_check_ref(dev_ptr);
 
         size_t max_alloc = sham::min(dev.prop.max_mem_alloc_size_dev, dev.prop.global_mem_size);
-        double max_size = double(max_alloc) / (vec4_size * 4);
+        double max_size  = double(max_alloc) / (vec4_size * 4); // there is 2 allocations so /4
 
         auto result = bench_step(N);
 

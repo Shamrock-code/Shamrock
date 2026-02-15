659e8fd819737f511c2d4eb9112e657f42bd21a4
David Fang <david.fang@ikmail.com>
2026-02-15 03:01:06 +0100

    add test for least_squares

--- src/tests/shammath/matrixTests.cpp
+++ src/tests/shammath/matrixTests.cpp
@@ -11,6 +11,7 @@
 #include "shamcomm/logs.hpp"
 #include "shammath/matrix.hpp"
 #include "shammath/matrix_op.hpp"
+#include "shammath/solve.hpp"
 #include "shamtest/details/TestResult.hpp"
 #include "shamtest/shamtest.hpp"
 
@@ -560,4 +561,50 @@ TestStart(Unittest, "shammath/matrix::Cholesky_solve", test_Cholesky_solve, 1) {
                    + sycl::pow(p1[2] - p2[2], 2)
                < 1e-9;
     });
+}
+
+// This test uses Eckerle4 from NIST Standard Reference Database [Eckerle, K., NIST (1979)]
+// https://www.itl.nist.gov/div898/strd/nls/data/eckerle4.shtml
+TestStart(Unittest, "shammath/solve::least_squares", test_least_squares, 1) {
+    std::vector<f64> p0 = {1, 1e1, 5e2};
+    std::vector<f64> X
+        = {400.000000e0, 405.000000e0, 410.000000e0, 415.000000e0, 420.000000e0, 425.000000e0,
+           430.000000e0, 435.000000e0, 436.500000e0, 438.000000e0, 439.500000e0, 441.000000e0,
+           442.500000e0, 444.000000e0, 445.500000e0, 447.000000e0, 448.500000e0, 450.000000e0,
+           451.500000e0, 453.000000e0, 454.500000e0, 456.000000e0, 457.500000e0, 459.000000e0,
+           460.500000e0, 462.000000e0, 463.500000e0, 465.000000e0, 470.000000e0, 475.000000e0,
+           480.000000e0, 485.000000e0, 490.000000e0, 495.000000e0, 500.000000e0};
+
+    std::vector<f64> Y = {
+        0.0001575e0, 0.0001699e0, 0.0002350e0, 0.0003102e0, 0.0004917e0, 0.0008710e0, 0.0017418e0,
+        0.0046400e0, 0.0065895e0, 0.0097302e0, 0.0149002e0, 0.0237310e0, 0.0401683e0, 0.0712559e0,
+        0.1264458e0, 0.2073413e0, 0.2902366e0, 0.3445623e0, 0.3698049e0, 0.3668534e0, 0.3106727e0,
+        0.2078154e0, 0.1164354e0, 0.0616764e0, 0.0337200e0, 0.0194023e0, 0.0117831e0, 0.0074357e0,
+        0.0022732e0, 0.0008800e0, 0.0004579e0, 0.0002345e0, 0.0001586e0, 0.0001143e0, 0.0000710e0};
+    auto ls = shammath::least_squares(
+        [](std::vector<f64> p, f64 x) -> f64 {
+            f64 b1 = p[0];
+            f64 b2 = p[1];
+            f64 b3 = p[2];
+            return (b1 / b2) * exp(-0.5 * ((x - b3) / b2) * ((x - b3) / b2));
+        },
+        X,
+        Y,
+        p0,
+        1000,
+        1e-9);
+
+    auto pfit                     = ls.first;
+    auto R2                       = ls.second;
+    std::vector<f64> res          = {pfit[0], pfit[1], pfit[2], R2};
+    std::vector<f64> ex_res       = {1.55, 4.08, 4.5154e2};
+    std::vector<f64> ex_deviation = {2e-2, 4.7e-2, 4.7e-2, 1e-2};
+
+    bool test_fit = true;
+    for (size_t i; i < 4; i++) {
+        if (sham::abs(res[i] - ex_res[i]) > ex_deviation[i]) {
+            test_fit = false;
+        }
+    };
+    REQUIRE(test_fit);
 }

d88a48194b69fb171d5e16be45109a604f01fc8e
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-02-15 13:17:39 +0100

    [CI] try to split up example gallery gen

---
+++ doc/sphinx/gen_sphinx_doc_single_example.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+if ! (which python &> /dev/null || which python3 &> /dev/null); then
+    echo "You need to have the python command available to generate the sphinx doc"
+    exit 1
+fi
+
+if ! python3 -c "import shamrock" &> /dev/null; then
+    echo "You need to have shamrock installed in your python path to generate the sphinx doc"
+    exit 1
+fi
+
+pip_list=(
+    "sphinx"
+    "pydata-sphinx-theme"
+    "sphinx-gallery"
+    "memory-profiler"
+    "sphinx-copybutton"
+    "sphinx_design"
+    "sphinxcontrib-video"
+    "sympy"
+    "matplotlib"
+    "numpy"
+    "scipy"
+    )
+
+for package in "${pip_list[@]}"; do
+    if [ -z "$(pip list | grep $package)" ]; then
+        echo "You need to have $package installed to generate the sphinx doc"
+        echo "Running : pip install $package"
+        pip install $package
+    else
+        echo "$package is installed."
+    fi
+done
+
+set -e
+
+cd "$(dirname "$0")"
+
+EXAMPLE_FILE="$1"
+echo "Using example file: ${EXAMPLE_FILE}"
+
+make html SPHINXOPTS="-D sphinx_gallery_conf.filename_pattern=${EXAMPLE_FILE}"
+
+set +e
+
+rm -rf examples/_to_trash

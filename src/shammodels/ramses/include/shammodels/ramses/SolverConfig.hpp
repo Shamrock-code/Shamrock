8ad43fc9bb0b074d83a0e1783560fb75d1e8b35b
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-02-19 00:21:57 +0100

    use the json adl serializer while where at it

--- src/shammodels/ramses/include/shammodels/ramses/SolverConfig.hpp
+++ src/shammodels/ramses/include/shammodels/ramses/SolverConfig.hpp
@@ -27,6 +27,7 @@
 #include "shammodels/common/amr/AMRBlock.hpp"
 #include "shamrock/experimental_features.hpp"
 #include "shamrock/io/units_json.hpp"
+#include <shamrock/io/json_std_optional.hpp>
 #include <shamunits/Constants.hpp>
 #include <shamunits/UnitSystem.hpp>
 #include <stdexcept>
@@ -320,27 +321,6 @@ struct shammodels::basegodunov::SolverConfig {
         }
     }
 };
-
-namespace nlohmann {
-    template<typename T>
-    struct adl_serializer<std::optional<T>> {
-        static void to_json(json &j, const std::optional<T> &opt) {
-            if (opt.has_value()) {
-                j = *opt;
-            } else {
-                j = nullptr;
-            }
-        }
-
-        static void from_json(const json &j, std::optional<T> &opt) {
-            if (j.is_null()) {
-                opt = std::nullopt;
-            } else {
-                opt = j.get<T>();
-            }
-        }
-    };
-} // namespace nlohmann
 
 namespace shammodels::basegodunov {
 

8294319f86019ada2002cdcce5aac980f6b05254
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2026-04-01 19:55:53 +0200

    updates

--- examples/TO_MIGRATE/ramses/liska_wendroff_amr.py
+++ examples/TO_MIGRATE/ramses/liska_wendroff_amr.py
@@ -17,14 +17,14 @@ multy = 1
 multz = 1
 max_amr_lev = 1
 cell_size = 2 << max_amr_lev  # refinement is limited to cell_size = 2
-base = 512
+base = 64
 scale_fact = 0.3 / (cell_size * base * multx)
 gamma = 1.4
 err_min = 0.30
 err_max = 0.10
 nx = base * 2
 ny = base * 2
-sim_folder = "_to_trash/ramses_liska_wendroff_03/"
+sim_folder = "_to_trash/ramses_liska_wendroff_128/bis/"
 if shamrock.sys.world_rank() == 0:
     os.makedirs(sim_folder, exist_ok=True)
 
@@ -54,20 +54,33 @@ def plot_rho_slice_cartesian(metadata, arr_rho_pos, iplot, case_name, dpi=200):
     ext = metadata["extent"]
 
     my_cmap = matplotlib.colormaps["viridis"].copy()  # copy the default cmap
-    my_cmap.set_bad(color="black")
+    my_cmap.set_bad(color="white")
 
     arr_rho_pos = np.array(arr_rho_pos).reshape(nx, ny)
 
     ampl = 1e-5
 
+    X = np.linspace(ext[0], ext[1], nx)
+    Y = np.linspace(ext[2], ext[3], ny)
+    X, Y = np.meshgrid(X, Y)
+
     plt.figure(dpi=dpi)
-    res = plt.imshow(
-        arr_rho_pos,
-        cmap=my_cmap,
-        origin="lower",
-        extent=ext,
-        aspect="auto",
-    )
+    vmin = 0
+    vmax = 1.15
+    levels = np.arange(vmin, vmax + 0.025, 0.025)
+    res = plt.contourf(X, Y, arr_rho_pos, levels=levels, cmap=my_cmap)
+    cs = plt.contour(X, Y, arr_rho_pos, levels=levels, colors="black", linewidths=0.5)
+
+    # plt.figure(dpi=dpi)
+    # res = plt.imshow(
+    #     arr_rho_pos,
+    #     cmap=my_cmap,
+    #     origin="lower",
+    #     extent=ext,
+    #     aspect="auto",
+    #     vmin=0.,
+    #     vmax=1.1
+    # )
     plt.xlabel("x")
     plt.ylabel("y")
     plt.title(f"t = {metadata['time']:0.3f} [seconds]")
@@ -91,14 +104,14 @@ def run_case(set_bc_func, case_name):
     cfg.set_scale_factor(scale_fact)
     set_bc_func(cfg)
     cfg.set_eos_gamma(gamma)
-    cfg.set_Csafe(0.3)
+    cfg.set_Csafe(0.5)
     cfg.set_riemann_solver_hllc()
-    cfg.set_slope_lim_minmod()
+    cfg.set_slope_lim_vanleer_sym()
     cfg.set_face_time_interpolation(True)
     # cfg.set_amr_mode_pseudo_gradient_based(error_min=err_min, error_max=err_max)
 
     model.set_solver_config(cfg)
-    model.init_scheduler(int(1e4), 1)
+    model.init_scheduler(int(1e7), 1)
     model.make_base_grid(
         (0, 0, 0), (cell_size, cell_size, cell_size), (base * multx, base * multy, base * multz)
     )
@@ -107,9 +120,9 @@ def run_case(set_bc_func, case_name):
 
         x, y, z = rmin
         if (x + y) < 0.15:
-            return 1
-        else:
             return 0.125
+        else:
+            return 1.0
 
     etot_L = 1.0 / (gamma - 1)
     etot_R = 0.14 / (gamma - 1)
@@ -120,9 +133,9 @@ def run_case(set_bc_func, case_name):
 
         x, y, z = rmin
         if (x + y) < 0.15:
-            return etot_L
-        else:
             return etot_R
+        else:
+            return etot_L
 
     def rhovel_map(rmin, rmax):
         return (0, 0, 0)
@@ -133,7 +146,7 @@ def run_case(set_bc_func, case_name):
 
     # model.evolve_once(0,0.1)
     fact = 200
-    tmax = 0.0127 * fact
+    tmax = 0.01 * fact
     all_t = np.linspace(0, tmax, fact)
 
     def plot(t, iplot):
@@ -143,7 +156,7 @@ def run_case(set_bc_func, case_name):
 
     current_time = 0.0
     for i, t in enumerate(all_t):
-        model.dump_vtk(os.path.join(sim_folder, f"{case_name}_{i:04d}.vtk"))
+        # model.dump_vtk(os.path.join(sim_folder, f"{case_name}_{i:04d}.vtk"))
         model.evolve_until(t)
         current_time = t
         plot(current_time, i)

be2dae52c3b879b170bea8f7ffa2cc8868783607
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2026-04-09 12:30:28 +0200

    updates of 1D intersting blast wave test

--- examples/TO_MIGRATE/ramses/interacting_blast_wave.py
+++ examples/TO_MIGRATE/ramses/interacting_blast_wave.py
@@ -17,7 +17,8 @@ def run_1d_interacting_blast_wave_test(
     nxbox,
     nybox,
 ):
-    timestamps = 100
+    timestamps = 20
+    timestep_lists = [0.010, 0.016, 0.026, 0.028, 0.030, 0.032, 0.034, 0.036, 0.038, 0.040, 0.042]
     gamma = 1.4
 
     output_folder = "_to_trash/interacting_blast_wave/"
@@ -47,8 +48,8 @@ def run_1d_interacting_blast_wave_test(
     cfg.set_slope_lim_vanleer_sym()
     cfg.set_face_time_interpolation(True)
     cfg.set_boundary_condition("x", "reflective")
-    cfg.set_boundary_condition("y", "periodic")
-    cfg.set_boundary_condition("z", "periodic")
+    cfg.set_boundary_condition("y", "reflective")
+    cfg.set_boundary_condition("z", "reflective")
 
     model.set_solver_config(cfg)
     model.init_scheduler(int(1e7), 1)
@@ -59,7 +60,7 @@ def run_1d_interacting_blast_wave_test(
     p0_l = 1000
     p0_m = 0.01
     p0_r = 100
-    tmax = 0.02
+
 
     def rho_map(rmin, rmax):
         return 1
@@ -80,74 +81,136 @@ def run_1d_interacting_blast_wave_test(
     model.set_field_value_lambda_f64("rhoetot", rhoetot_map)
     model.set_field_value_lambda_f64_3("rhovel", rhovel_map)
 
-    dt_evolve = tmax / timestamps
+
 
     def analysis():
         results = []
-        for i in range(timestamps + 1):
-            model.evolve_until(dt_evolve * i)
-            rho_vals = model.render_slice("rho", "f64", positions_plot)
-            results.append(rho_vals)
+        for dt in timestep_lists:
+            model.evolve_until(dt)
+            rho_vals  = model.render_slice("rho", "f64", positions_plot)
+            rhov_vals = model.render_slice("rhovel", "f64_3", positions_plot)
+            rhoetot_vals = model.render_slice("rhoetot", "f64", positions_plot)
+            vx = np.array(rhov_vals)[:, 0] / np.array(rho_vals)
+            P = (np.array(rhoetot_vals) - 0.5 * np.array(rho_vals) * vx**2) * (gamma - 1)
+            e_int = P/((gamma - 1.) * np.array(rho_vals))
+            results_dic = {
+            "rho": np.array(rho_vals),
+            "vx": np.array(vx),
+            "P": np.array(P),
+            "e_int":np.array(e_int)
+             }
+            results.append(results_dic)
+       
+            output = np.column_stack((np.array(rho_vals), np.array(vx), np.array(P), np.array(e_int)))
+            filename= f"data_interacting_blast_wave_reso_{2*base*multx}_at_{dt}.txt"
+            np.savetxt(os.path.join(output_folder,filename),
+                       output,
+                       fmt=["%.10f", "%.10f", "%.10f", "%.10f"],
+                       header="rho vx P e_int",
+                       )
         return results
 
-    def plot_results(data, tmax):
+    def plot_results(data):
         arr_x = [x[0] for x in positions_plot]
         for i, frame in enumerate(data):
-            fig, axs = plt.subplots(1, 1, figsize=(8, 8))
-            fig.suptitle(f"Interacting blast wave test at t = {dt_evolve * i}", fontsize=14)
-            axs.set_xlabel("$x$")
-            axs.set_ylabel("$\\rho$")
-            axs.grid(True, alpha=0.3)
-            axs.plot(arr_x, frame, label=f"{base * 2}", linewidth=1)
-            axs.legend()
+            fig, axs = plt.subplots(2, 2, figsize=(8, 8), dpi=100, constrained_layout=True)
+         
+            ## density         
+            axs[0,0].set_xlabel("$x$")
+            axs[0,0].set_ylabel("$\\rho$")
+            axs[0,0].grid(True, alpha=0.3)
+            axs[0,0].scatter(arr_x, frame["rho"], label=f"{2*base*multx}", s=10, marker = "*")
+            axs[0,0].legend(loc=0)
+
+            ## velocity
+            axs[0,1].set_xlabel("$x$")
+            axs[0,1].set_ylabel("$v_\\mathrm{x}$")
+            axs[0,1].grid(True, alpha=0.3)
+            axs[0,1].scatter(arr_x, frame["vx"], label=f"{2*base*multx}", s=10, marker ="*" )
+            axs[0,1].legend(loc=0)
+            
+            
+            ## pressure
+            axs[1,1].set_xlabel("$x$")
+            axs[1,1].set_ylabel("$\\mathrm{P}$")
+            axs[1,1].grid(True, alpha=0.3)
+            axs[1,1].scatter(arr_x, frame["P"], label=f"{2*base*multx}", s=10, marker ="*" )
+            axs[1,1].legend(loc=0)
+
+            ## internal energy
+            axs[1,0].set_xlabel("$x$")
+            axs[1,0].set_ylabel("$\\mathrm{e}_\\mathrm{int}$")
+            axs[1,0].grid(True, alpha=0.3)
+            axs[1,0].scatter(arr_x, frame["e_int"], label=f"{2*base*multx}", s=10, marker ="*" )
+            axs[1,0].legend(loc=0)
+        
             plt.tight_layout()
-            plt.savefig(os.path.join(output_folder, f"Interacting_blast_wave_test_{i:04d}.png"))
+            plt.savefig(os.path.join(output_folder, f"Interacting_blast_wave_test_at_{timestep_lists[i]}_nx_reso_{base*2*multx}.png"))
             plt.close(fig)
 
-    def gif_results(data, tmax, case_anim="inter-blast"):
+    # def gif_results(data, tmax, case_anim="inter-blast"):
 
-        arr_x = [x[0] for x in positions_plot]
+    #     arr_x = [x[0] for x in positions_plot]
 
-        import matplotlib.animation as animation
+    #     import matplotlib.animation as animation
 
-        fig2, axs = plt.subplots(1, 1, figsize=(8, 8))
-        fig2.suptitle(f"{case_anim} - t = {0.0:.3f} s", fontsize=14)
+    #     fig2, axs = plt.subplots(2, 2, figsize=(8, 8))
+    #     fig2.suptitle(f"{case_anim} - t = {0.0:.3f} s", fontsize=14)
+    #     ax_rho, ax_vx, ax_P = axs
 
-        # Calculate global min/max across all frames for fixed y-axis limits
-        rho_min = min(np.min(frame) for frame in data)
-        rho_max = max(np.max(frame) for frame in data)
-        # Add 5% margin to y-axis limits
-        rho_margin = (rho_max - rho_min) * 0.05
-        # Configure each axis
-        axs.set_xlabel("$x$")
-        axs.set_ylabel("$\\rho$")
-        axs.set_ylim(rho_min - rho_margin, rho_max + rho_margin)
-        axs.grid(True, alpha=0.3)
-        (line_rho,) = axs.plot(arr_x, data[0], label="$\\rho$", linewidth=2, color="C0")
-        axs.legend()
+    #     # Calculate global min/max across all frames for fixed y-axis limits
+    #     rho_min = min(np.min(frame["rho"]) for frame in data)
+    #     rho_max = max(np.max(frame["rho"]) for frame in data)
+    #     vx_min = min(np.min(frame["vx"]) for frame in data)
+    #     vx_max = max(np.max(frame["vx"]) for frame in data)
+    #     P_min = min(np.min(frame["P"]) for frame in data)
+    #     P_max = max(np.max(frame["P"]) for frame in data)
+    #     eint_min = min(np.min(frame["e_int"]) for frame in data)
+    #     eint_max = max(np.max(frame["e_int"]) for frame in data)
 
-        def animate(frame):
-            t = tmax * frame / timestamps
-            line_rho.set_ydata(data[frame])
+    #     # Add 5% margin to y-axis limits
+    #     rho_margin = (rho_max - rho_min) * 0.05
+    #     vx_margin = (vx_max - vx_min) * 0.05
+    #     P_margin = (P_max -P_min) * 0.05
+    #     eint_margin = (eint_max - eint_min) * 0.05
 
-            fig2.suptitle(f"{case_anim} - t = {t:.3f} s", fontsize=14)
-            return (line_rho, 0)
+    #     # Configure each axis
+    #     axs[0,0].set_xlabel("$x$")
+    #     axs[0,0].set_ylabel("$\\rho$")
+    #     axs[0,0].set_ylim(rho_min - rho_margin, rho_max + rho_margin)
+    #     axs[0,0].grid(True, alpha=0.3)
 
-        anim = animation.FuncAnimation(
-            fig2, animate, frames=timestamps + 1, interval=50, blit=False, repeat=True
-        )
-        plt.tight_layout()
-        writer = animation.PillowWriter(fps=15, metadata=dict(artist="Me"), bitrate=1800)
-        anim.save(os.path.join(output_folder, "Interacting_blast_wave_test.gif"), writer=writer)
-        return anim
+    #     axs[0,1].set_xlabel("$x$")
+    #     axs[0,1].set_ylabel("$v_\\mathrm{x}$")
+    #     axs[0,1].set_ylim(vx_min - vx_margin, vx_max + vx_margin)
+    #     axs[0,1].grid(True, alpha=0.3)
+
+
+    #     (line_rho,) = axs[0,0].plot(arr_x, data[0], label="$\\rho$", linewidth=2, color="C0")
+    #     axs[0,0].legend()
+
+    #     def animate(frame):
+    #         t = tmax * frame / timestamps
+    #         line_rho.set_ydata(data[frame])
+
+    #         fig2.suptitle(f"{case_anim} - t = {t:.3f} s", fontsize=14)
+    #         return (line_rho, 0)
+
+    #     anim = animation.FuncAnimation(
+    #         fig2, animate, frames=timestamps + 1, interval=50, blit=False, repeat=True
+    #     )
+    #     plt.tight_layout()
+    #     writer = animation.PillowWriter(fps=15, metadata=dict(artist="Me"), bitrate=1800)
+    #     anim.save(os.path.join(output_folder, "Interacting_blast_wave_test.gif"), writer=writer)
+    #     return anim
 
     def run_and_plot():
 
         data = analysis()
 
-        return plot_results(data, tmax), gif_results(data, tmax)
+        return plot_results(data)
 
-    plot, anim = run_and_plot()
+    plot = run_and_plot()
 
 
-run_1d_interacting_blast_wave_test(16, 2, 1)
+run_1d_interacting_blast_wave_test(8, 2<<2, 1)

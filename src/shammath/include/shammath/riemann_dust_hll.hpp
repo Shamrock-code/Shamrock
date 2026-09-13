77d6be00c61be5d83aec07b5e787629c02a2af89
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-09-13 10:57:47 +0200

    [Math] make riemann solver variable naming consistent

--- src/shammath/include/shammath/riemann_dust_hll.hpp
+++ src/shammath/include/shammath/riemann_dust_hll.hpp
@@ -27,30 +27,30 @@ namespace shammath {
      * hydrodynamics, Appendice E
      * @tparam FSpec
      * @param fspec dust state spec (flux/vn operations, no equation of state)
-     * @param primL left  primitive state
-     * @param primR right primitive state
+     * @param prim_l left  primitive state
+     * @param prim_r right primitive state
      * @param n face unit normal
      */
     template<DustFluidStateSpec FSpec>
     inline constexpr typename FSpec::Tcons d_hll_flux(
         const FSpec &fspec,
-        const typename FSpec::Tprim &primL,
-        const typename FSpec::Tprim &primR,
+        const typename FSpec::Tprim &prim_l,
+        const typename FSpec::Tprim &prim_r,
         const typename FSpec::Tvec &n) {
         using Tscal = typename FSpec::Tscal;
         using Tcons = typename FSpec::Tcons;
 
-        const Tscal vnL = fspec.vn(primL, n);
-        const Tscal vnR = fspec.vn(primR, n);
-        const Tscal S   = sham::max(sham::abs(vnL), sham::abs(vnR));
+        const Tscal vn_l = fspec.vn(prim_l, n);
+        const Tscal vn_r = fspec.vn(prim_r, n);
+        const Tscal s    = sham::max(sham::abs(vn_l), sham::abs(vn_r));
 
-        const Tcons fL = fspec.flux(primL, n, vnL);
-        const Tcons fR = fspec.flux(primR, n, vnR);
+        const Tcons f_l = fspec.flux(prim_l, n, vn_l);
+        const Tcons f_r = fspec.flux(prim_r, n, vn_r);
 
-        const Tcons cL = fspec.prim_to_cons(primL);
-        const Tcons cR = fspec.prim_to_cons(primR);
+        const Tcons c_l = fspec.prim_to_cons(prim_l);
+        const Tcons c_r = fspec.prim_to_cons(prim_r);
 
-        return 0.5 * ((fL + fR) - S * (cR - cL));
+        return 0.5 * ((f_l + f_r) - s * (c_r - c_l));
     }
 
 } // namespace shammath
